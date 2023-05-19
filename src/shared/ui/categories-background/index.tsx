c2eaed4f7b77b0c4259590572de9da2e85775adb
Tina Tezadova <ftezadova@gmail.com>
2023-05-19 21:17:53 +0300

    feat: добавить компонент "подложка для категорий"

---
+++ src/shared/ui/categories-background/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import classnames from "classnames";
+import styles from "./styles.module.css";
+
+interface CategoriesBackgroundProps {
+  extClassName?: string;
+  theme: "success" | "primary" | "warning" | "secondary";
+  content?: string | React.ReactNode;
+  size?: "medium" | "large";
+}
+
+export const CategoriesBackground = ({
+  extClassName,
+  theme = "primary",
+  content,
+  size = "large",
+}: CategoriesBackgroundProps) => (
+  <div
+    className={classnames(
+      styles["categories-background"],
+      styles[`categories-background--${size}`],
+      styles[`categories-background--${theme}`],
+      extClassName
+    )}
+  >
+    {content}
+  </div>
+);
