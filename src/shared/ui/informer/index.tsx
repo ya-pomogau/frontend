808c31e4b98561f74620a917961470ab80e6c95f
Tina Tezadova <ftezadova@gmail.com>
2023-05-23 12:35:10 +0300

    feat: add informer

---
+++ src/shared/ui/informer/index.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import classnames from "classnames";
+import styles from "./styles.module.css";
+import HeartIcon from "./assets/heart-icon.svg";
+
+interface InformerProps {
+  extClassName?: string;
+  text?: string;
+  icon?: React.ReactNode;
+}
+
+export const Informer = ({ text, extClassName, icon }: InformerProps) => (
+  <div
+    className={classnames(
+      styles.informer,
+      styles[text ? "informer--with-text" : ""],
+      extClassName
+    )}
+  >
+    <div className={styles["informer-img-block"]}>
+      {icon || <img src={HeartIcon} alt="Сердeчное рукопожатие" />}
+    </div>
+    <span
+      className={classnames(
+        "text-inter",
+        "text_size_large",
+        styles["informer-text"]
+      )}
+    >
+      {text}
+    </span>
+  </div>
+);
