b453a9d18f0997ff22d70ae82e0d3ad5a0bc507f
Aleksandr Dronov <dronovspb@gmail.com>
2023-05-31 21:47:44 +0300

    feat: add yandex map

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import App from "app";
+import { YMaps } from "@pbe/react-yandex-maps";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <StrictMode>
-    <App />
+    <YMaps query={{ lang: 'en_RU', load: "Map,Placemark,map.addon.balloon,geoObject.addon.balloon" }}>
+      <App />
+    </YMaps>
   </StrictMode>
 );
---
+++ src/shared/ui/map/index.tsx
@@ -0,0 +1,42 @@
+import { Map } from "@pbe/react-yandex-maps";
+import { Data } from "./types";
+import { Mark } from "./Mark";
+
+type Props = {
+  width?: string | number;
+  height?: string | number;
+  mapSettings?: {
+    latitude: number;
+    longitude: number;
+    zoom: number;
+  };
+  tasks?: Data[];
+  onClick?: () => void;
+};
+
+export const YandexMap = ({
+  width = 500,
+  height = 500,
+  mapSettings = { latitude: 59.93, longitude: 30.31, zoom: 15 },
+  onClick,
+  tasks,
+}: Props) => (
+  <div>
+    <Map
+      state={{
+        center: [mapSettings.latitude, mapSettings.longitude],
+        zoom: mapSettings.zoom,
+      }}
+      options={{
+        suppressMapOpenBlock: true,
+        yandexMapDisablePoiInteractivity: true,
+      }}
+      width={width}
+      height={height}
+    >
+      {tasks?.map((task) => (
+        <Mark {...task} onClick={onClick} key={task.id}/>
+      ))}
+    </Map>
+  </div>
+);
