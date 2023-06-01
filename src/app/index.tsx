9423351d7c4b3d3278cc0861d149ec2a50a5ac8f
vvvolkova-92 <saiviolet@gmail.com>
2023-06-01 14:40:22 +0300

    FIELDSET / Storybook

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,32 +1,8 @@
 import "./assets/styles/index.css";
 import { withProviders } from "./providers";
-import Fieldset from "../shared/ui/fieldset";
-import Checkbox from "../shared/ui/checkbox";
-import { FieldsetView } from "../shared/ui/fieldset/utils";
-import RadioButton from "../shared/ui/radio-button";
+import { AppRoutes } from "../pages";
 
-// function App() {
-//   return <AppRoutes />;
-// }
 function App() {
-  return (
-    <>
-      <Fieldset title="Тут длинный текст" view={FieldsetView.COLUMN}>
-        <Checkbox label="Test1" id="11" name="1" />
-        <Checkbox label="Test2" id="12" name="1" />
-        <Checkbox label="Test3" id="13" name="1" />
-      </Fieldset>
-      <Fieldset title="Тут длинный текст" view={FieldsetView.TWO_COLUMN}>
-        <Checkbox label="Test1" id="21" name="2" />
-        <Checkbox label="Test2" id="22" name="2" />
-        <Checkbox label="Test3" id="23" name="2" />
-      </Fieldset>
-      <Fieldset title="Тут длинный текст" view={FieldsetView.ROW}>
-        <RadioButton label="Test1" id="31" name="3" />
-        <RadioButton label="Test2" id="32" name="3" />
-        <RadioButton label="Test3" id="33" name="3" />
-      </Fieldset>
-    </>
-  );
+  return <AppRoutes />;
 }
 export default withProviders(App);
--- src/shared/ui/fieldset/utils.ts
+++ src/shared/ui/fieldset/utils.ts
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-shadow
 export enum FieldsetView {
   COLUMN = "COLUMN",
-  TWO_COLUMN = "TWO_COLUMN",
+  TWO_COLUMNS = "TWO_COLUMNS",
   ROW = "ROW",
 }
