7da83a2718af70656a7dcbf910e9fa2f4e76f40b
nikolay <n.smolov@yandex.ru>
2023-06-04 00:46:38 +0300

    feat: adds header

---
+++ src/widgets/header/navigation/types.tsx
@@ -0,0 +1,21 @@
+export interface ISideBarElementPosition {
+  flexDirection: "row" | "column" | "row-reverse";
+  justifyContent: "flex-start" | "center" | "flex-end";
+  gap: number;
+  textAlign: "center" | "right" | "left";
+}
+export interface ISideBarElementProps {
+  title: string;
+  to: string;
+  icon?: any;
+  position?: ISideBarElementPosition;
+}
+export interface ISideBarPosition {
+  ulflexDirection: "row" | "column" | "row-reverse";
+  ulgap: number;
+  element: ISideBarElementPosition;
+}
+export interface ISideBarProps {
+  links: ISideBarElementProps[];
+  position: ISideBarPosition;
+}
