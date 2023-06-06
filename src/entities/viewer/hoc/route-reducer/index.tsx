25d6b20642845020c675d9e9c7fe2a37f8622cf8
kapitan4ever <stklmn@yandex.ru>
2023-06-07 00:20:21 +0300

    add hoc for role

--- src/entities/viewer/hoc/route-reducer/index.tsx
+++ src/entities/viewer/hoc/route-reducer/index.tsx
@@ -1,5 +1,32 @@
+import React from "react";
+import { useAppSelector } from "app/hooks";
+import { RootState } from "app/store";
 import { Navigate } from "react-router-dom";
+import { TRole } from "../../types";
+
+interface Props {
+  role: TRole;
+}
+
+const withRoleRedirect = (WrappedComponent: React.ComponentType<any>) => {
+  const WithRoleRedirect: React.FC<Props> = ({ role, ...props }) => {
+    const viewerRole = useAppSelector((state: RootState) => state.viewer.role);
+
+    const shouldRedirect = viewerRole !== role;
+
+    if (shouldRedirect) {
+      return <Navigate to={`${role}`} />;
+    }
+
+    return <WrappedComponent {...props} />;
+  };
+
+  return WithRoleRedirect;
+};
+
+export default withRoleRedirect;
 
 export function ProfileReducer() {
-  return <Navigate to="volunteer/active" />;
+  const viewerRole = useAppSelector((state: RootState) => state.viewer.role);
+  return <Navigate to={viewerRole} />;
 }
