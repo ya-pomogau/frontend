ddfe74ccad709839c076d40706a5f2dea1914266
Elana Tollu <elana.tollu@yandex.ru>
2023-07-01 01:25:26 +0300

    add registration form

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -7,10 +7,20 @@ import { Icon } from "shared/ui/icons";
 import { useMediaQuery } from "shared/hooks";
 import { ButtonContainer } from "shared/ui/button-container";
 import { CardButton } from "shared/ui/card-button";
+import { Input } from "shared/ui/input";
+import { useState } from "react";
+
+import { Button } from "shared/ui/button";
+import { VkIcon } from "shared/ui/icons/vk-icon";
 import styles from "./styles.module.css";
+
+
 
 export function RegisterPage() {
   const isMobile = useMediaQuery("(max-width:1150px)");
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [address, setAddress] = useState('');
 
   return (
     <PageLayout
@@ -58,18 +68,57 @@ export function RegisterPage() {
       }
       content={
         <ContentLayout
-                heading={
-                  <SmartHeader
-                    settingIcon={
-                      <Icon color="blue" icon="RegistrationIcon" size="54" />
-                    }
-                    settingText="Регистрация"
-                    extClassName={styles.header}
-                  />
+            heading={
+                <SmartHeader
+                settingIcon={
+                    <Icon color="blue" icon="RegistrationIcon" size="54" />
                 }
-              >
-                lkfjkgkjtkgktgt
-              </ContentLayout>
+                settingText="Регистрация"
+                extClassName={styles.header}
+                />
+            }
+            >
+            <form className={styles.form}>
+                <p className={styles.title}>Зарегистрироваться</p>
+
+                <Input
+                    extClassName={styles.field}
+                    label="ФИО"
+                    name="name"
+                    value={name}
+                    onChange={(event) => setName(event.currentTarget.value)}
+                    placeholder="ФИО"
+                    type="text"
+                />
+
+                <Input
+                    extClassName={styles.field}
+                    label="Телефон"
+                    name="phone"
+                    value={phone}
+                    onChange={(event) => setPhone(event.currentTarget.value)}
+                    placeholder="+7 (000) 000 00 00"
+                    type="tel"
+                />
+
+                <Input
+                    extClassName={styles.field}
+                    label="Адрес"
+                    name="address"
+                    value={address}
+                    onChange={(event) => setAddress(event.currentTarget.value)}
+                    placeholder="ул. Нахимова, д.9, у подъезда №3"
+                    type="text"
+                />
+
+                <Button
+                    buttonType="primary"
+                    customIcon={<VkIcon color="white" size="24"/>}
+                    label="Зарегистрироваться через ВКонтакте"
+                    size="extraLarge"
+                />
+            </form>
+        </ContentLayout>
       }
     />
   );
