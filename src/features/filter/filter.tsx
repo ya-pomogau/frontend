c080cd3c9011523fe8bf52f8371e5300be3ee27c
Platon Brazhnikov <platonbrazhnikov123@gmail.com>
2023-08-24 23:31:04 +0400

    fixed filter display

--- src/features/filter/filter.tsx
+++ src/features/filter/filter.tsx
@@ -44,15 +44,7 @@ export const Filter = ({ items, notFoundFilter = false }: FilterProps) => {
   };
 
   if (notFoundFilter) {
-    return (
-      <FilterWrapper
-        filterMenu={
-          <span>Для такого вида профиля волонтёра нет подходящего фильтра</span>
-        }
-        filterValues={filterValues}
-        setFilterValues={setFilterValues}
-      />
-    );
+    return null;
   }
 
   return (
