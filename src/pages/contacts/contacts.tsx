4b8ce9f68f73407edab5de28f32eef0722eafdb7
Gyxer513 <fpanchuk@yandex.ru>
2023-08-30 16:06:48 +0300

    start fix

--- src/pages/contacts/contacts.tsx
+++ src/pages/contacts/contacts.tsx
@@ -1,5 +1,6 @@
+import React, { ChangeEvent } from 'react';
+import { useAppSelector } from 'app/hooks';
 import classNames from 'classnames';
-
 import { ContentLayout } from 'shared/ui/content-layout';
 import { Icon } from 'shared/ui/icons';
 import { PageLayout } from 'shared/ui/page-layout';
@@ -11,6 +12,15 @@ import { SideMenuLink } from 'widgets/side-menu/components/side-menu-link';
 import styles from './styles.module.css';
 
 export function ContactsPage() {
+  const focus = false;
+  const { role } = useAppSelector((state) => state.user);
+  const [userData, setUserData] = React.useState({
+    userEmail: 'www@yandex.ru',
+    userVKLink: 'https://vk.com/me2help',
+  });
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setUserData({ ...userData, [e.target.name]: e.target.value });
+  };
   return (
     <PageLayout
       side={
@@ -42,29 +52,39 @@ export function ContactsPage() {
             />
           }
         >
-          <div className={styles.container}>
-            <h2
-              className={classNames(
-                'text',
-                'text_size_large',
-                'text_type_regular',
-                styles.title
-              )}
-            >
-              Эл. почта
-            </h2>
-            <a
-              href="mailto:www@yandex.ru"
-              className={classNames(
-                'text',
-                'text_size_large',
-                'text_type_regular',
-                styles.link
-              )}
-            >
-              www@yandex.ru
-            </a>
-          </div>
+          <form className={styles.container}>
+            <div className={styles.element_box}>
+              <h2
+                className={classNames(
+                  'text',
+                  'text_size_large',
+                  'text_type_regular',
+                  styles.title
+                )}
+              >
+                Эл. почта
+              </h2>
+              <a
+                href="mailto:www@yandex.ru"
+                className={classNames(
+                  'text',
+                  'text_size_large',
+                  'text_type_regular',
+                  styles.link
+                )}
+              >
+                <input
+                  type="text"
+                  className={styles.input}
+                  onChange={onChange}
+                  autoFocus={focus}
+                  name="userEmail"
+                  defaultValue={userData.userEmail}
+                  disabled={true}
+                />
+              </a>
+            </div>
+          </form>
           <div className={styles.container}>
             <h2
               className={classNames(
@@ -87,7 +107,7 @@ export function ContactsPage() {
               target="_blank"
               rel="noreferrer"
             >
-              https://vk.com/me2help
+              {userData.userVKLink}
             </a>
           </div>
         </ContentLayout>
