137be9f2c67fd8803ae13c96fe307aa7ed54ab59
Zdthns <Zdthns25@gmail.com>
2023-10-11 11:19:25 +0300

    correction of an error in the text

--- src/widgets/header/utils.tsx
+++ src/widgets/header/utils.tsx
@@ -66,7 +66,7 @@ export const linksMenuMobileUnauthorized: ISideBarElementProps[] = [
     icon: <WriteMessageIcon color="blue" />,
   },
   {
-    title: 'Политика конфеденциальности',
+    title: 'Политика конфиденциальности',
     to: '/policy',
     icon: <LockIcon color="blue" />,
   },
