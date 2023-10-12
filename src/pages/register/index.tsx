df89dd801b080be602b8ea961c9281f497126302
marussia1996 <mariashirokova1996@gmail.com>
2023-10-12 11:56:49 +0300

    change register page && add change content

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,17 +1,24 @@
 import { FormEvent, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { SmartHeader } from 'shared/ui/smart-header';
 import { Icon } from 'shared/ui/icons';
 import { Input } from 'shared/ui/input';
 import { Button } from 'shared/ui/button';
 import { VkIcon } from 'shared/ui/icons/vk-icon';
-
-import styles from './styles.module.css';
 import { InputAddress } from 'shared/ui/input-address';
 
+import styles from './styles.module.css';
+
 export function RegisterPage() {
+  const navigate = useNavigate();
+
+  const [showLoginButton, setShowLoginButton] = useState(false);
+  const [changeContent, setChangeContent] = useState(false);
+
   const [name, setName] = useState('');
   const [phone, setPhone] = useState('');
+  const [role, setRole] = useState('');
 
   const [address, setAddress] = useState<{
     address: string;
@@ -23,8 +30,7 @@ export function RegisterPage() {
 
   const onSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-
-    console.log('Региcтрация:', name, phone, address);
+    console.log('Региcтрация:', name, phone, address, role);
   };
 
   const handleAddressValueChange = (
@@ -36,7 +42,12 @@ export function RegisterPage() {
       coords: coords || [],
     });
   };
-
+  const redirectToLogin = () => {
+    navigate('/login');
+  };
+  const toggleLoginButton = () => {
+    setShowLoginButton(true);
+  };
   return (
     <>
       <SmartHeader
@@ -44,54 +55,98 @@ export function RegisterPage() {
         text="Регистрация"
         extClassName={styles.header}
       />
-      <p className={styles.title}>Зарегистрироваться</p>
-
-      <form className={styles.form} onSubmit={onSubmit}>
-        <Input
-          extClassName={styles.field}
-          required
-          label="ФИО"
-          name="name"
-          value={name}
-          onChange={(event) => setName(event.currentTarget.value)}
-          placeholder="ФИО"
-          type="text"
-        />
-
-        <Input
-          extClassName={styles.field}
-          required
-          label="Телефон"
-          name="phone"
-          value={phone}
-          onChange={(event) => setPhone(event.currentTarget.value)}
-          placeholder="+7 (000) 000 00 00"
-          type="tel"
-          pattern="^[+]7 \(\d{3}\) \d{3} \d{2} \d{2}$"
-          title="+7 (123) 456 78 90"
-        />
-
-        <div>
-          <InputAddress
+      <p
+        className={changeContent ? styles.titlePrimary : styles.titleAdditional}
+      >
+        Зарегистрироваться
+      </p>
+      {!changeContent && (
+        <div className={styles.wrapper}>
+          <div className={styles.buttonContainer}>
+            <Button
+              buttonType="primary"
+              actionType="button"
+              label="Хочу помочь"
+              size="extraLarge"
+              onClick={() => {
+                setChangeContent(true);
+                setRole('volunteer');
+              }}
+            />
+            <Button
+              buttonType="secondary"
+              actionType="button"
+              label="Нужна помощь"
+              size="extraLarge"
+              onClick={() => {
+                setChangeContent(true);
+                setRole('recipient');
+              }}
+            />
+          </div>
+          <a className={styles.link} onClick={toggleLoginButton}>
+            Уже есть аккаунт
+          </a>
+          {showLoginButton && (
+            <Button
+              buttonType="primary"
+              actionType="submit"
+              customIcon={<VkIcon color="white" size="24" />}
+              label="Войти через ВКонтакте"
+              size="extraLarge"
+              onClick={() => redirectToLogin()}
+            />
+          )}
+        </div>
+      )}
+      {changeContent && (
+        <form className={styles.form} onSubmit={onSubmit}>
+          <Input
+            extClassName={styles.field}
             required
-            name="address"
-            address={address}
-            setAddress={handleAddressValueChange}
+            label="ФИО"
+            name="name"
+            value={name}
+            onChange={(event) => setName(event.currentTarget.value)}
+            placeholder="ФИО"
+            type="text"
           />
 
-          <p className={styles.text}>
-            Укажите адрес и мы подберем ближайшее к вам задание
-          </p>
-        </div>
+          <Input
+            extClassName={styles.field}
+            required
+            label="Телефон"
+            name="phone"
+            value={phone}
+            onChange={(event) => setPhone(event.currentTarget.value)}
+            placeholder="+7 (000) 000 00 00"
+            type="tel"
+            pattern="^[+]7 \(\d{3}\) \d{3} \d{2} \d{2}$"
+            title="+7 (123) 456 78 90"
+          />
 
-        <Button
-          buttonType="primary"
-          actionType="submit"
-          customIcon={<VkIcon color="white" size="24" />}
-          label="Зарегистрироваться через ВКонтакте"
-          size="extraLarge"
-        />
-      </form>
+          <div>
+            <InputAddress
+              required
+              name="address"
+              address={address}
+              setAddress={handleAddressValueChange}
+            />
+
+            <p className={styles.text}>
+              Укажите адрес и мы подберем ближайшее к вам задание
+            </p>
+          </div>
+
+          <Button
+            buttonType="primary"
+            actionType="submit"
+            customIcon={<VkIcon color="white" size="24" />}
+            label="Зарегистрироваться через ВКонтакте"
+            size="extraLarge"
+          />
+        </form>
+      )}
     </>
   );
 }
