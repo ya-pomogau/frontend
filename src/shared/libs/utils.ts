f398d2611b7ceefdb13bce598feccb6a6cfd723d
Vladislav Kireev <kireew.vladislaw@gmail.com>
2023-10-14 19:19:30 +0300

    fix: merge conflict

--- src/shared/libs/utils.ts
+++ src/shared/libs/utils.ts
@@ -3,6 +3,19 @@ import differenceInMilliseconds from 'date-fns/differenceInMilliseconds';
 export const isTaskUrgent = (date: string): boolean =>
   differenceInMilliseconds(new Date(date), new Date()) < 86400000;
 
+export const getFullQueriesForYApi = (
+  mainJSApi: string,
+  additionalApi: { [name: string]: { enable: boolean; apiKey: string } } = {}
+): string => {
+  let additionalQueries = '';
+  for (const key in additionalApi) {
+    if (additionalApi[key].enable) {
+      additionalQueries = '&' + key + '=' + additionalApi[key].apiKey;
+    }
+  }
+  return mainJSApi + additionalQueries;
+};
+
 export const isEmptyObj = (obj: object): boolean => {
   for (const key in obj) {
     return false;
