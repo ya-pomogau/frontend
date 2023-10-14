5e0aa991b428462a940203efdeb462cef7748599
Vladislav Kireev <kireew.vladislaw@gmail.com>
2023-10-14 19:03:15 +0300

    feat: added helper

--- src/shared/libs/utils.ts
+++ src/shared/libs/utils.ts
@@ -2,3 +2,10 @@ import differenceInMilliseconds from 'date-fns/differenceInMilliseconds';
 
 export const isTaskUrgent = (date: string): boolean =>
   differenceInMilliseconds(new Date(date), new Date()) < 86400000;
+
+export const isEmptyObj = (obj: object): boolean => {
+  for (const key in obj) {
+    return false;
+  }
+  return true;
+};
