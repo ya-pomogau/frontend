4c344579e60a0a8fac121de9b82f582d528813dd
Vladislav Kireev <kireew.vladislaw@gmail.com>
2023-10-19 20:37:32 +0300

    fix: fixed doubled PageLayout menu

--- src/pages/chats-conflicts/index.tsx
+++ src/pages/chats-conflicts/index.tsx
@@ -1,8 +1,5 @@
 import styles from './styles.module.css';
 
-import { PageLayout } from '../../shared/ui/page-layout';
-import { UserInfo } from '../../entities/user';
-import { SideMenu, SideMenuLink } from '../../widgets/side-menu';
 import { ContentLayout } from '../../shared/ui/content-layout';
 import { SmartHeader } from '../../shared/ui/smart-header';
 import { PageSubMenuForChats } from '../../widgets/page-sub-menu';
@@ -29,91 +26,49 @@ export function ConflictsPage() {
   const isMobile = useMediaQuery('(max-width:800px)');
 
   return (
-    <PageLayout
-      // side={
-      //   <>
-      //     <div className={styles.user}>
-      //       <UserInfo />
-      //     </div>
-      //     <SideMenu
-      //       authRequired={false}
-      //       extClassName={styles.button_container}
-      //       links={
-      //         <>
-      //           <SideMenuLink
-      //             to="/"
-      //             icon={<Icon color="white" icon="BlockIcon" size="54" />}
-      //             text="Подтверждение/Блокировка"
-      //           />
-      //           <SideMenuLink
-      //             to="/"
-      //             icon={<Icon color="white" icon="StatisticIcon" size="54" />}
-      //             text="Статистика"
-      //           />
-      //           <SideMenuLink
-      //             to="/"
-      //             icon={
-      //               <Icon color="white" icon="CreateApplication" size="54" />
-      //             }
-      //             text="Заявки/Работа с заявками"
-      //           />
-      //         </>
-      //       }
-      //     />
-      //   </>
-      // }
-      content={
-        <ContentLayout
-          extClassName={styles.content}
-          heading={
-            <SmartHeader
-              icon={<Icon color="blue" icon="ReadMessageIcon" size="54" />}
-              text="Чат"
-            />
-          }
-        >
-          <PageSubMenuForChats />
-          {isMobile ? (
-            <>
-              <ChatsList
-                selectedChatId={selectedConflictId}
-                onSelectChat={setSelectedConflictId}
-                handleNavigate={handleNavigate}
-              />
-              <Routes>
-                <Route
-                  path={'/:conflictId'}
-                  element={
-                    <Conflict
-                      isMobile={isMobile}
-                      onClose={() => closeConflict()}
-                    />
-                  }
-                />
-              </Routes>
-            </>
-          ) : (
-            <div className={styles.container}>
-              <ChatsList
-                selectedChatId={selectedConflictId}
-                onSelectChat={setSelectedConflictId}
-                handleNavigate={handleNavigate}
-              />
-              <Routes>
-                <Route
-                  path={'/:conflictId'}
-                  element={
-                    <Conflict
-                      isMobile={isMobile}
-                      onClose={() => closeConflict()}
-                    />
-                  }
-                />
-              </Routes>
-            </div>
-          )}
-        </ContentLayout>
+    <ContentLayout
+      extClassName={styles.content}
+      heading={
+        <SmartHeader
+          icon={<Icon color="blue" icon="ReadMessageIcon" size="54" />}
+          text="Чат"
+        />
       }
-    />
+    >
+      <PageSubMenuForChats />
+      {isMobile ? (
+        <>
+          <ChatsList
+            selectedChatId={selectedConflictId}
+            onSelectChat={setSelectedConflictId}
+            handleNavigate={handleNavigate}
+          />
+          <Routes>
+            <Route
+              path={'/:conflictId'}
+              element={
+                <Conflict isMobile={isMobile} onClose={() => closeConflict()} />
+              }
+            />
+          </Routes>
+        </>
+      ) : (
+        <div className={styles.container}>
+          <ChatsList
+            selectedChatId={selectedConflictId}
+            onSelectChat={setSelectedConflictId}
+            handleNavigate={handleNavigate}
+          />
+          <Routes>
+            <Route
+              path={'/:conflictId'}
+              element={
+                <Conflict isMobile={isMobile} onClose={() => closeConflict()} />
+              }
+            />
+          </Routes>
+        </div>
+      )}
+    </ContentLayout>
   );
 }
--- src/pages/chats-waiting/index.tsx
+++ src/pages/chats-waiting/index.tsx
@@ -2,14 +2,9 @@ import { Routes, Route, useNavigate } from 'react-router-dom';
 
 import { ContentLayout } from 'shared/ui/content-layout';
 import { Icon } from 'shared/ui/icons';
-import { PageLayout } from 'shared/ui/page-layout';
 import { SmartHeader } from 'shared/ui/smart-header';
 
-import { SideMenu } from 'widgets/side-menu';
-import { SideMenuLink } from 'widgets/side-menu/components/side-menu-link';
-
 import styles from './styles.module.css';
-import { UserInfo } from '../../entities/user';
 import { PageSubMenuForChats } from '../../widgets/page-sub-menu';
 import { ChatsList } from '../../widgets/chats/components';
 import { useState } from 'react';
@@ -31,85 +26,45 @@ export function ChatsWaitingPage() {
   const isMobile = useMediaQuery('(max-width:1150px)');
 
   return (
-    <PageLayout
-      // side={
-      //   <>
-      //     <div className={styles.user}>
-      //       <UserInfo />
-      //     </div>
-      //     <SideMenu
-      //       authRequired={false}
-      //       extClassName={styles.button_container}
-      //       links={
-      //         <>
-      //           <SideMenuLink
-      //             to="/"
-      //             icon={<Icon color="white" icon="BlockIcon" size="54" />}
-      //             text="Подтверждение/Блокировка"
-      //           />
-      //           <SideMenuLink
-      //             to="/"
-      //             icon={<Icon color="white" icon="StatisticIcon" size="54" />}
-      //             text="Статистика"
-      //           />
-      //           <SideMenuLink
-      //             to="/"
-      //             icon={
-      //               <Icon color="white" icon="CreateApplication" size="54" />
-      //             }
-      //             text="Заявки/Работа с заявками"
-      //           />
-      //         </>
-      //       }
-      //     />
-      //   </>
-      // }
-      content={
-        <ContentLayout
-          extClassName={styles.content}
-          heading={
-            <SmartHeader
-              icon={<Icon color="blue" icon="ReadMessageIcon" size="54" />}
-              text="Чат"
-            />
-          }
-        >
-          <PageSubMenuForChats />
-          {isMobile ? (
-            <>
-              <ChatsList
-                selectedChatId={selectedChatId}
-                onSelectChat={setSelectedChatId}
-                handleNavigate={handleNavigate}
-              />
-              <Routes>
-                <Route
-                  path={'/:chatId'}
-                  element={
-                    <Chat onClose={() => closeChat()} isMobile={isMobile} />
-                  }
-                />
-              </Routes>
-            </>
-          ) : (
-            <div className={styles.container}>
-              <ChatsList
-                selectedChatId={selectedChatId}
-                onSelectChat={setSelectedChatId}
-                handleNavigate={handleNavigate}
-              />
-              <Routes>
-                <Route
-                  path={'/:chatId'}
-                  element={
-                    <Chat isMobile={isMobile} onClose={() => closeChat()} />
-                  }
-                />
-              </Routes>
-            </div>
-          )}
-        </ContentLayout>
+    <ContentLayout
+      extClassName={styles.content}
+      heading={
+        <SmartHeader
+          icon={<Icon color="blue" icon="ReadMessageIcon" size="54" />}
+          text="Чат"
+        />
       }
-    />
+    >
+      <PageSubMenuForChats />
+      {isMobile ? (
+        <>
+          <ChatsList
+            selectedChatId={selectedChatId}
+            onSelectChat={setSelectedChatId}
+            handleNavigate={handleNavigate}
+          />
+          <Routes>
+            <Route
+              path={'/:chatId'}
+              element={<Chat onClose={() => closeChat()} isMobile={isMobile} />}
+            />
+          </Routes>
+        </>
+      ) : (
+        <div className={styles.container}>
+          <ChatsList
+            selectedChatId={selectedChatId}
+            onSelectChat={setSelectedChatId}
+            handleNavigate={handleNavigate}
+          />
+          <Routes>
+            <Route
+              path={'/:chatId'}
+              element={<Chat isMobile={isMobile} onClose={() => closeChat()} />}
+            />
+          </Routes>
+        </div>
+      )}
+    </ContentLayout>
   );
 }
