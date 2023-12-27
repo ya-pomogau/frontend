a0b3da12ba10ffa2ec0b54c4879610902828fb03
Zdthns <Zdthns25@gmail.com>
2023-12-27 18:13:03 +0300

    redirect

--- src/widgets/header/menu/Logout/LogoutButton.tsx
+++ src/widgets/header/menu/Logout/LogoutButton.tsx
@@ -3,13 +3,15 @@ import styles from './styles.module.css';
 import { Icon } from 'shared/ui/icons';
 import { logoutUser } from 'entities/user/model';
 import { useAppDispatch } from 'app/hooks';
+import { useNavigate } from 'react-router-dom';
 
 export const LogoutButton = () => {
   const dispatch = useAppDispatch();
-
+  const navigate = useNavigate();
   const handlerOnClick = () => {
     console.log('exit');
     dispatch(logoutUser());
+    return navigate('/');
   };
 
   return (
