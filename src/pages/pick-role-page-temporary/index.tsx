b27bb350530a8d4da6f6f8be0f2fd565648e1d13
Yulia Berezhnaya <p.kotta@yandex.ru>
2023-12-27 13:22:42 +0300

    Добавлена возможность выбора роли не одобренного админом волонтера

--- src/pages/pick-role-page-temporary/index.tsx
+++ src/pages/pick-role-page-temporary/index.tsx
@@ -48,7 +48,7 @@ export function PickRolePage() {
   };
 
   const getUnconfirmedRecipient = () => {
-    dispatch(setUserRole('recipient'));
+    dispatch(setUserRole(UserRole.RECIPIENT));
     setUserId(9);
   };
 
