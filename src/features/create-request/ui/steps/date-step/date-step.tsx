52c8c2825dee94575ade25da094b0f6dff8665f1
me@vladbarsukov.ru <me@vladbarsukov.ru>
2024-01-11 19:58:45 +0300

    Исправил валидацию времени, теперь при смене даты валидация отрабатывает корректно

--- src/features/create-request/ui/steps/date-step/date-step.tsx
+++ src/features/create-request/ui/steps/date-step/date-step.tsx
@@ -47,12 +47,14 @@ export const DateStep = ({ isMobile }: IDateStepProps) => {
   useEffect(() => {
     const currentTime = new Date(); // текущая дата
     const currentFormattedTime = format(currentTime, 'HH:mm'); // привожу в нужный формат
-    if (time && time < currentFormattedTime) {
+    const currentDate = new Date().toLocaleDateString(); // получаем текущую дату в формате "дд.мм.гггг"
+
+    if (time && time < currentFormattedTime && date && currentDate === date) {
       dispatch(setDateValidation(true));
     } else {
       dispatch(setDateValidation(false));
     } //сравниваю даты
-  }, [time]);
+  }, [time, date]);
 
   useEffect(() => {
     if (termlessRequest) {
