d68d027e4e7d8ddc6b740a7dadb2283f62bac8ce
Artur Karamov <karamov.1311@yandex.ru>
2024-01-13 21:05:18 +0300

    volunteer tasks 6 and 8

--- src/entities/task/ui/task-list/index.tsx
+++ src/entities/task/ui/task-list/index.tsx
@@ -148,6 +148,7 @@ export const TaskList = ({
                 handleClickEditButton={
                   isStatusActive ? handleClickEditButton : undefined
                 }
+                handleClickConflictButton={() => 2}
               />
             </li>
           ))}
