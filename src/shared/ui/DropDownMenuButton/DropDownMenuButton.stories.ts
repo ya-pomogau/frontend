7e8d9bebe7fda86e1027cb3ce8f1dcafd15e6a2d
Zdthns <Zdthns25@gmail.com>
2024-01-14 12:46:26 +0300

    storybook

--- src/shared/ui/DropDownMenuButton/DropDownMenuButton.stories.ts
+++ src/shared/ui/DropDownMenuButton/DropDownMenuButton.stories.ts
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { DropDownMenuButton } from '.';
+import './DropDownMenuButton.module.css';
 
 const meta: Meta<typeof DropDownMenuButton> = {
   title: 'uikit/Buttons/DropDownMenuButton',
@@ -11,17 +12,19 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
+  name: 'adminMessage',
   args: {
     children: 'Написать администратору',
     onClick: () => console.log('Кнопка нажата'),
     buttonType: 'adminMessage',
+    isDisabled: false,
+    isMobile: false,
   },
 };
 
 export const Logout: Story = {
   args: {
     children: 'Выход',
-    onClick: () => console.log('Кнопка нажата'),
-    buttonType: undefined,
+    onClick: () => console.log('Выход из аккаунта'),
   },
 };
