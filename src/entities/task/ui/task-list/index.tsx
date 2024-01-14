0b21c40f6daf50d9cb3bae08a4c4af20a3d0f493
Alexandr Gerasimov <phantom6391@yandex.ru>
2024-01-14 18:02:39 +0400

    Отрисовка модалки

--- src/entities/task/ui/task-list/index.tsx
+++ src/entities/task/ui/task-list/index.tsx
@@ -10,6 +10,10 @@ import type { UserRole } from 'entities/user/types';
 import type { Task } from 'entities/task/types';
 
 import styles from './styles.module.css';
+import { useAppSelector } from 'app/hooks';
+import { useState } from 'react';
+import { Dialog } from 'shared/ui/dialog';
+import { Tooltip } from 'shared/ui/tooltip';
 
 interface TaskListProps {
   userRole?: UserRole | null;
@@ -31,10 +35,16 @@ export const TaskList = ({
   handleClickAddTaskButton,
 }: TaskListProps) => {
   const buttonGuard = usePermission([CONFIRMED], 'recipient');
-
+  const [isOpen, setIsOpen] = useState(false);
   const handleDeniedAccess = () => {
-    alert('Вам пока нельзя такое, дождитесь проверки администратором');
+    setIsOpen(!isOpen);
+    isOpen &&
+      console.log('Вам пока нельзя такое, дождитесь проверки администратором');
+    !isOpen && console.log('Закрыли');
   };
+  const isConfirmed = useAppSelector((store) => {
+    return store.user.data?.status === CONFIRMED;
+  });
 
   return (
     <>
@@ -114,8 +124,16 @@ export const TaskList = ({
                 onClick={
                   buttonGuard ? handleClickAddTaskButton : handleDeniedAccess
                 }
+                extClassName={styles.add_task_icon_unconf}
                 size="large"
               />
+              <Tooltip
+                visible={isOpen}
+                extClassName={styles.modal}
+                pointerPosition="center"
+              >
+                <div>Здесь будет модалка</div>
+              </Tooltip>
             </>
           )}
         </div>
--- src/pages/profile-active/index.tsx
+++ src/pages/profile-active/index.tsx
@@ -8,12 +8,15 @@ import { openPopup } from 'features/create-request/model';
 import { Request } from 'features/create-request';
 import { useGetTasksByStatusQuery } from 'services/tasks-api';
 import { Loader } from 'shared/ui/loader';
+import { CONFIRMED } from 'shared/libs/statuses';
 
 export function ProfileActivePage() {
   const dispatch = useAppDispatch();
   const { data: tasks, isLoading } = useGetTasksByStatusQuery('active');
   const { role } = useAppSelector((state) => state.user);
-
+  const isConfirmed = useAppSelector((state) => {
+    return state.user.data?.status === CONFIRMED;
+  });
   const isMobile = useMediaQuery('(max-width:1150px)');
 
   const { isPopupOpen } = useAppSelector((store) => store.createRequest);
@@ -55,7 +58,7 @@ export function ProfileActivePage() {
           isMobile={isMobile}
           handleClickAddTaskButton={() => dispatch(openPopup())}
           isStatusActive
-          tasks={tasks}
+          tasks={isConfirmed ? tasks : []}
           isLoading={isLoading}
         />
       )}
