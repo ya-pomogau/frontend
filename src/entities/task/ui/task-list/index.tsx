cad34350cb6d7b5c0e998e00d9264037068589bf
Alexandr Gerasimov <phantom6391@yandex.ru>
2024-01-14 20:33:33 +0400

    Позиционирование модалки

--- src/entities/task/ui/task-list/index.tsx
+++ src/entities/task/ui/task-list/index.tsx
@@ -15,7 +15,6 @@ import { useEffect, useRef, useState } from 'react';
 
 import { CloseCrossIcon } from 'shared/ui/icons/close-cross-icon';
 import { Tooltip } from 'shared/ui/tooltip';
-import { CloseCrossIcon } from 'shared/ui/icons/close-cross-icon';
 
 interface TaskListProps {
   userRole?: UserRole | null;
