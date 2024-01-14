1bf0aaed7af4fc3474c5f2834a3ee467442c6a57
Alexandr Gerasimov <phantom6391@yandex.ru>
2024-01-14 13:45:42 +0400

    Добавил новую сущность в дб

--- src/pages/pick-role-page-temporary/index.tsx
+++ src/pages/pick-role-page-temporary/index.tsx
@@ -46,6 +46,11 @@ export function PickRolePage() {
     dispatch(setUserRole('recipient'));
   };
 
+  const getUnconfirmedRecipient = () => {
+    dispatch(setUserRole('recipient'));
+    setUserId(8);
+  };
+
   const getAdminRole = () => {
     dispatch(setUserRole('admin'));
     setUserId(2);
@@ -131,6 +136,12 @@ export function PickRolePage() {
         <li>
           <button onClick={getRecipientRole} style={{ marginRight: 10 }}>
             Получить роль рецепиента.
+          </button>
+        </li>
+
+        <li>
+          <button onClick={getUnconfirmedRecipient} style={{ marginRight: 10 }}>
+            Получить роль неактивированного рецепиента.
           </button>
         </li>
 
