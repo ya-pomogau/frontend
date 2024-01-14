1def52a525dd3d0e93348b1d12686bf832e1ec6d
Alexandr Gerasimov <phantom6391@yandex.ru>
2024-01-14 16:12:11 +0400

    Создание сущности и получение сообщения

--- src/shared/ui/page-layout/index.tsx
+++ src/shared/ui/page-layout/index.tsx
@@ -9,6 +9,8 @@ import { FeedbackSideMenu, SideMenuForAuthorized } from 'widgets/side-menu';
 import { useLocation } from 'react-router-dom';
 import { ErrorDialog } from '../error-dialog';
 import { NoConectionPage } from 'features/error-boundary/pages/NoConectionPage';
+import { UNCONFIRMED } from 'shared/libs/statuses';
+import { RegistrationNotice } from '../registration-notice';
 
 interface PageLayoutProps {
   content?: ReactNode;
@@ -16,20 +18,27 @@ interface PageLayoutProps {
 
 export const PageLayout = ({ content }: PageLayoutProps) => {
   const { isError, errorText } = useAppSelector((state) => state.error);
-  // const isLoadingUserData = useAppSelector((state) => state.user.isLoading);
-  //const isLoadingTasksData = useAppSelector((state) => state.tasks.isLoading);
+  const isLoadingUserData = useAppSelector((state) => state.user.isLoading);
+  const isUnConfirmedUser = useAppSelector((state) => {
+    return (state.user.data && state.user.data.status === UNCONFIRMED) || null;
+  });
+  const isRole = useAppSelector((state) => state.user.data?.role);
+  // TODO: Добавить другие случаи сообщений (потеря связи и пр.)
+  const hasMessage = isUnConfirmedUser;
   const location = useLocation();
   console.log(`PageLayout - isError: ${isError}`);
 
   return (
     <>
-      {/* {isLoadingUserData && <Loader />} */}
+      {isLoadingUserData && <Loader />}
       {location.pathname === '/policy' ||
       location.pathname === '/blog' ||
       location.pathname === '/pick' ? (
         <div className={styles.content}> {content} </div>
       ) : (
-        <div className={styles.main}>
+        <div
+          className={styles.main + ' ' + (hasMessage && styles.mainWithMessage)}
+        >
           <div className={styles.side}>
             <div className={styles.user}>
               <UserInfo />
@@ -41,6 +50,11 @@ export const PageLayout = ({ content }: PageLayoutProps) => {
               <SideMenuForAuthorized />
             )}
           </div>
+          {isUnConfirmedUser && isRole === 'Recipient' && (
+            <div className={styles.message}>
+              <RegistrationNotice settingText="Спасибо за регистрацию. Как только администратор подтвердит Вашу учетную запись, Вы сможете создавать заявки." />
+            </div>
+          )}
           <div className={styles.content}>
             {isError && <ErrorDialog text={errorText}></ErrorDialog>}
             {errorText !== 'Ошибка подключения' ? content : <NoConectionPage />}
