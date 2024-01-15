b2fd8144e7348d25f61ac53f87bfb95e9ca40990
Zdthns <Zdthns25@gmail.com>
2024-01-15 11:52:32 +0300

    start StoryBook for DropDownMtyu

--- src/widgets/header/DropDownMenu/DropDownMenu.stories.ts
+++ src/widgets/header/DropDownMenu/DropDownMenu.stories.ts
@@ -3,6 +3,19 @@ import { DropDownMenu } from '.';
 import './styles.module.css';
 
 const meta: Meta<typeof DropDownMenu> = {
+  title: 'widgets/DropDownMenu',
   component: DropDownMenu,
   tags: ['autodocs'],
+};
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  name: 'adminMessage',
+  args: {
+    menuActive: true,
+    //children: '',
+    //isDisabled: false,
+    //isMobile: false,
+  },
 };
