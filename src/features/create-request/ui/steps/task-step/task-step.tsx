53c513102b4d1fcb68dd9e28a5fee85a5d20f52b
Alexandr Gerasimov <phantom6391@yandex.ru>
2024-01-15 21:42:43 +0400

    Валидация на кнопке

--- src/features/create-request/ui/steps/task-step/task-step.tsx
+++ src/features/create-request/ui/steps/task-step/task-step.tsx
@@ -12,16 +12,32 @@ import Dropdown, { Option } from '../../../../../shared/ui/dropdown';
 
 import styles from './task-step.module.css';
 import usePropsButtonCustom from '../useButtonPropsCustom';
+import { useGetTasksByStatusQuery } from 'services/tasks-api';
 
 interface ITaskStepProps {
   isMobile?: boolean;
 }
 
 export const TaskStep = ({ isMobile }: ITaskStepProps) => {
-  const { descriptionForTask, categories, category, isTypeEdit } = useAppSelector(
-    (state) => state.createRequest
-  );
+  const { descriptionForTask, categories, category, isTypeEdit } =
+    useAppSelector((state) => state.createRequest);
+  const userId = useAppSelector((state) => state.user.data?.id);
+  const { data: tasks } = useGetTasksByStatusQuery('active');
+  // console.log(tasks);
+  // Фильтруем заявки по id
+  const taskId = tasks.filter((item: any) => {
+    if (item.recipient.id === userId) {
+      return item;
+    }
+  });
+  // Получаем id категории
+  const categoryId = taskId.map((item: any) => item.category.id);
+  //Получем объект уже выбранной категории
+  const commonIds = categories.filter((obj) => categoryId.includes(obj.id));
 
+  // console.log(taskId);
+  console.log(categoryId);
+  console.log(commonIds);
   const dispatch = useAppDispatch();
 
   const optionsForSelect = categories?.map((item) => ({
@@ -117,6 +133,9 @@ export const TaskStep = ({ isMobile }: ITaskStepProps) => {
           {descriptionForTask.length <= 5 && (
             <p className={styles.messageAlert}>Добавьте описание задачи</p>
           )}
+          {category.value === '' && category.label === '' && (
+            <p className={styles.messageAlert}>Выберите тип задачи</p>
+          )}
         </div>
         {category.value === '' && category.label === '' && (
             <p className={styles.messageAlert}>Выберите тип задачи</p>
@@ -124,8 +143,8 @@ export const TaskStep = ({ isMobile }: ITaskStepProps) => {
         {!isTypeEdit && (
           <Button
             buttonType="secondary"
-            label={propsButton.backlabel}
-            onClick={propsButton.backonClick}
+            label={propsButton.label}
+            onClick={propsButton.onClick}
             extClassName={styles.prevButton}
           />
         )}
