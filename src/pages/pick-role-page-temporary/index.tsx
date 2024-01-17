d470a81df4a31f54a8dcdda2e2523f72226f5fb3
Alexandr Gerasimov <phantom6391@yandex.ru>
2024-01-17 19:19:43 +0400

    Изменение ID

--- src/pages/pick-role-page-temporary/index.tsx
+++ src/pages/pick-role-page-temporary/index.tsx
@@ -59,7 +59,7 @@ export function PickRolePage() {
 
   const getUnconfirmedRecipient = () => {
     dispatch(setUserRole('recipient'));
-    setUserId(8);
+    setUserId(9);
   };
 
   const getAdminRole = () => {
