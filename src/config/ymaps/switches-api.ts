c01909149d65cada1552afb090667a65c143850e
Alexandr Gerasimov <phantom6391@yandex.ru>
2024-01-17 20:16:37 +0400

    Изменил env, добавил env.example

--- src/config/ymaps/switches-api.ts
+++ src/config/ymaps/switches-api.ts
@@ -1,2 +1,9 @@
 /* eslint-disable prettier/prettier */
-export const YMAPS_SUGGEST_SWITCHER = true;
+// export const YMAPS_SUGGEST_SWITCHER = process.env.REACT_APP_SUGGEST_SWITCHER;
+
+const switcherValue = process.env.REACT_APP_SUGGEST_SWITCHER;
+// Преобразование строки в булево значение
+export const YMAPS_SUGGEST_SWITCHER = switcherValue === 'true';
+console.log(YMAPS_SUGGEST_SWITCHER);
+console.log(`Это env ${typeof process.env.REACT_APP_SUGGEST_SWITCHER}`);
+console.log(process.env);
