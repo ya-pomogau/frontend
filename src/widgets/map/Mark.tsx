336d2e9411e600ffcaf061080e671f6f73c586bb
Alexandr Gerasimov <97280804+AlexandrGerasimov63@users.noreply.github.com>
2024-01-18 15:15:38 +0400

    Яндекс карта для реципиента (#109)
    * Сделали чтобы балун не открывался и перетаскивался
    
    * Работоспособный вариант карты
    
    * Правки типизации
    
    * Изменил env, добавил env.example
    
    * Вынес map key в env

--- src/widgets/map/Mark.tsx
+++ src/widgets/map/Mark.tsx
@@ -9,10 +9,13 @@ import './styles.css';
 import usePermission from 'shared/hooks/use-permission';
 import { ACTIVATED, CONFIRMED, VERIFIED } from 'shared/libs/statuses';
 import { UserRole } from 'shared/types/common.types';
+import { GeoCoordinates } from 'shared/types/point-geojson.types';
+import { setAddress } from 'features/create-request/model';
+import { useAppDispatch } from 'app/hooks';
 
 type MarkProps = {
   id?: number;
-  coordinates?: [number, number];
+  coordinates?: GeoCoordinates;
   isUrgentTask?: boolean;
   fullName?: string;
   phone?: string;
@@ -24,6 +27,8 @@ type MarkProps = {
   title?: string;
   date?: string;
   time?: string;
+  hasBalloon?: boolean;
+  draggable?: boolean;
 };
 
 export const Mark = React.memo(
@@ -41,8 +46,12 @@ export const Mark = React.memo(
     isAuthorised,
     date,
     time,
+    hasBalloon,
+    draggable,
   }: MarkProps) => {
-    const ymaps = useYMaps(['templateLayoutFactory']);
+    const ymaps = useYMaps(['templateLayoutFactory', 'geocode']);
+
+    const dispatch = useAppDispatch();
 
     const isGranted = usePermission(
       [CONFIRMED, ACTIVATED, VERIFIED],
@@ -257,6 +266,8 @@ export const Mark = React.memo(
           hideIconOnBalloonOpen: false,
           balloonOffset: [-158, 66],
           balloonPanelMaxMapArea: 0,
+          hasBalloon: hasBalloon,
+          draggable: draggable,
         }}
         properties={{
           isUrgentTask,
@@ -269,6 +280,24 @@ export const Mark = React.memo(
           isAuthorised,
           date,
           time,
+        }}
+        // Данный пропс отвечает за возможность получить координату в конце перетаскивания баллуна
+        onDragEnd={(event) => {
+          const newCoordinates = event.get('target').geometry.getCoordinates();
+          // С помощью геокодера конвертируем полученную координату в адрес и отправляем в стор createRequst
+          if (ymaps) {
+            const geo = ymaps.geocode(newCoordinates);
+            geo.then((res) => {
+              const firstGeoObject = res.geoObjects.get(0);
+
+              dispatch(
+                setAddress({
+                  additinalAddress: firstGeoObject.getAddressLine(),
+                  coords: newCoordinates,
+                })
+              );
+            });
+          }
         }}
       />
     );
