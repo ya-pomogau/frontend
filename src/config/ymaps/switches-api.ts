71c666573f24f11d85c5af4799298092ab5a6014
Alexandr Gerasimov <phantom6391@yandex.ru>
2024-01-18 15:02:07 +0400

    Вынес map key в env

--- src/config/ymaps/switches-api.ts
+++ src/config/ymaps/switches-api.ts
@@ -1,9 +1,3 @@
-/* eslint-disable prettier/prettier */
-// export const YMAPS_SUGGEST_SWITCHER = process.env.REACT_APP_SUGGEST_SWITCHER;
-
 const switcherValue = process.env.REACT_APP_SUGGEST_SWITCHER;
 // Преобразование строки в булево значение
 export const YMAPS_SUGGEST_SWITCHER = switcherValue === 'true';
-console.log(YMAPS_SUGGEST_SWITCHER);
-console.log(`Это env ${typeof process.env.REACT_APP_SUGGEST_SWITCHER}`);
-console.log(process.env);
