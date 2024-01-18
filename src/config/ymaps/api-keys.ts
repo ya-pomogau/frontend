4b71e7318fca45a17026b5d3b10127f3bf69d3b1
Alexandr Gerasimov <phantom6391@yandex.ru>
2024-01-18 15:02:07 +0400

    Вынес map key в env

--- src/config/ymaps/api-keys.ts
+++ src/config/ymaps/api-keys.ts
@@ -1,4 +1,6 @@
-export const YMAPS_API_KEY = '7b4bb7c4-4062-46a0-b7ab-f53610cc3365'; // JS APi
+export const YMAPS_API_KEY = process.env.REACT_APP_YANDEX_MAP_KEY
+  ? process.env.REACT_APP_YANDEX_MAP_KEY
+  : ''; // JS APi
 export const YMAPS_SUGGEST_API_KEY = process.env.REACT_APP_SUGGEST_SWITCHER_KEY
   ? process.env.REACT_APP_SUGGEST_SWITCHER_KEY
   : ''; // Suggest
