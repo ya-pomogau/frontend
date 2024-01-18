d40bc0f76db4f1e03d404da75e020aabeb3c55e9
Alexandr Gerasimov <phantom6391@yandex.ru>
2024-01-18 19:59:53 +0400

    Удалил Union

--- src/features/create-request/ui/steps/address-step/address-step.tsx
+++ src/features/create-request/ui/steps/address-step/address-step.tsx
@@ -35,7 +35,7 @@ export const AddressStep = ({ isMobile }: IAddressProps) => {
 
   const handleAddressValueChange = (
     additinalAddress: string,
-    coords?: GeoCoordinates | []
+    coords?: GeoCoordinates
   ) => {
     dispatch(setAddress({ additinalAddress, coords }));
   };
