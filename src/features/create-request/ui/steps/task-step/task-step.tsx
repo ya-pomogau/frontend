e8abc3282956b83d15ab4989598b509c6ce62b81
Alexandr Gerasimov <phantom6391@yandex.ru>
2024-01-24 11:00:36 +0400

    Правки типизации и положения алертов, удаление лишнего кода

--- src/features/create-request/ui/steps/task-step/task-step.tsx
+++ src/features/create-request/ui/steps/task-step/task-step.tsx
@@ -20,59 +20,19 @@ import { CloseCrossIcon } from 'shared/ui/icons/close-cross-icon';
 interface ITaskStepProps {
   isMobile?: boolean;
 }
-interface Coords {
-  right: number;
-  top: number;
-}
+
 export const TaskStep = ({ isMobile }: ITaskStepProps) => {
   const { descriptionForTask, categories, category } = useAppSelector(
     (state) => state.createRequest
   );
-  // const [isOpen, setIsOpen] = useState(false);
-  // const userId = useAppSelector((state) => state.user.data?.id);
-  // const { data: tasks } = useGetTasksByStatusQuery('active');
-  // console.log(tasks);
-  // // Фильтруем заявки по id
-  // const taskId = tasks.filter((item: any) => {
-  //   if (item.recipient.id === userId) {
-  //     return item;
-  //   }
-  // });
-  // Получаем id категории
-  // const categoryId = taskId.map((item: any) => item.category.id);
-  // //Получем объект уже выбранной категории
-  // const commonIds = categories.filter((obj) => categoryId.includes(obj.id));
-  // const commonSelected = commonIds?.map((item) => ({
-  //   value: String(item.id),
-  //   label: item.name,
-  // }));
-  // // console.log(taskId);
-  // // console.log(categoryId);
-  // // console.log(commonIds);
-  // console.log(commonSelected);
+
   const dispatch = useAppDispatch();
-
-  // const [popupPosion, setPopupPosion] = useState<Coords | null>(null);
-  // const optionRef = useRef<HTMLLIElement>(null);
-
-  // const getCoords = () => {
-  //   console.log(optionRef);
-  //   console.log(window.innerHeight);
-  //   const box = optionRef.current?.getBoundingClientRect();
-  //   console.log(box);
-  //   if (box) {
-  //     setPopupPosion({
-  //       right: window.innerWidth - box.right,
-  //       top: box.top + box.height,
-  //     });
-  //   }
-  // };
 
   const optionsForSelect = categories?.map((item) => ({
     value: String(item.id),
     label: item.name,
   }));
-  // console.log(optionsForSelect);
+
   const handleTaskValueChange = (item: Option) => {
     dispatch(setCategory(item));
   };
@@ -91,13 +51,6 @@ export const TaskStep = ({ isMobile }: ITaskStepProps) => {
     dispatch(changeStepDecrement());
   };
 
-  // const handlePopupOpen = () => {
-  //   if (!isOpen) {
-  //     getCoords();
-  //   }
-  //   setIsOpen((prev) => !prev);
-  // };
-
   const disabledBtn = () => {
     if (descriptionForTask.length <= 5 || descriptionForTask.length > 300) {
       return true;
@@ -106,14 +59,6 @@ export const TaskStep = ({ isMobile }: ITaskStepProps) => {
       return true;
     }
   };
-
-  // useEffect(() => {
-  //   window.addEventListener('resize', getCoords);
-
-  //   return () => {
-  //     window.removeEventListener('resize', getCoords);
-  //   };
-  // }, []);
 
   return (
     <div className={styles.mainWrapper}>
@@ -127,9 +72,7 @@ export const TaskStep = ({ isMobile }: ITaskStepProps) => {
                 'm-0',
                 styles.task
               )}
-            >
-              Дело
-            </p>
+            ></p>
             <div className={styles.headerWrapper} />
             <Dropdown
               selected={category}
@@ -139,6 +82,7 @@ export const TaskStep = ({ isMobile }: ITaskStepProps) => {
               items={optionsForSelect}
               extClassName={styles.select}
             />
+
             <TextArea
               value={descriptionForTask}
               label="Опишите задачу"
@@ -159,6 +103,9 @@ export const TaskStep = ({ isMobile }: ITaskStepProps) => {
               items={optionsForSelect}
               extClassName={styles.select}
             />
+            {category.value === '' && category.label === '' && (
+              <p className={styles.messageAlert}>Выберите тип задачи</p>
+            )}
             <TextArea
               value={descriptionForTask}
               label="Опишите задачу"
@@ -168,27 +115,6 @@ export const TaskStep = ({ isMobile }: ITaskStepProps) => {
               extClassName={styles.textarea}
               maxLength={300}
             />
-            {/* {isOpen && (
-              <Tooltip
-                visible
-                changeVisible={() => setIsOpen(false)}
-                elementStyles={{
-                  position: 'absolute',
-                  top: `${popupPosion?.top}px`,
-                  right: `${popupPosion?.right}px`,
-                }}
-              >
-                <div className={styles.closeWrapper}>
-                  <CloseCrossIcon
-                    className={styles.closeIcon}
-                    size="14"
-                    color="blue"
-                    onClick={() => setIsOpen(false)}
-                  />
-                </div>
-                Здесь будет текст
-              </Tooltip>
-            )} */}
           </>
         )}
       </div>
@@ -196,9 +122,6 @@ export const TaskStep = ({ isMobile }: ITaskStepProps) => {
         <div className={styles.alertWrapper}>
           {descriptionForTask.length <= 5 && (
             <p className={styles.messageAlert}>Добавьте описание задачи</p>
-          )}
-          {category.value === '' && category.label === '' && (
-            <p className={styles.messageAlert}>Выберите тип задачи</p>
           )}
         </div>
         <Button
