569242a2428a37b145243b1f7fc08be6d54ec01a
Artur Karamov <karamov.1311@yandex.ru>
2024-01-24 22:58:18 +0300

    add modalcontent widget

---
+++ src/widgets/task-buttons-content/types.tsx
@@ -0,0 +1,13 @@
+export enum ButtonType {
+  close = 'close',
+  conflict = 'conflict',
+  confirm = 'confirm',
+  phone = 'phone',
+  cancel = 'cancel',
+}
+
+export enum ReasonType {
+  first = 'first',
+  second = 'second',
+  third = 'third',
+}
