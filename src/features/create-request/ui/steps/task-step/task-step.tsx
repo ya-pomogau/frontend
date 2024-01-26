1271d94d07ba7424fe7d0a3997c2e1eddb35edc3
Alexandr Gerasimov <phantom6391@yandex.ru>
2024-01-26 13:25:13 +0400

    Правки

--- src/features/create-request/ui/steps/task-step/task-step.tsx
+++ src/features/create-request/ui/steps/task-step/task-step.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import classNames from 'classnames';
 
 import { useAppDispatch, useAppSelector } from 'app/hooks';
@@ -12,9 +12,6 @@ import Dropdown, { Option } from '../../../../../shared/ui/dropdown';
 
 import styles from './task-step.module.css';
 import usePropsButtonCustom from '../useButtonPropsCustom';
-import { useGetTasksByStatusQuery } from 'services/tasks-api';
-import { Tooltip } from 'shared/ui/tooltip';
-import { CloseCrossIcon } from 'shared/ui/icons/close-cross-icon';
 
 interface ITaskStepProps {
   isMobile?: boolean;
