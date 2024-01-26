a37c966c9b4611ab5d1a6c740dcef1b67bffb53e
Alexandr Gerasimov <phantom6391@yandex.ru>
2024-01-26 13:25:13 +0400

    Правки

--- src/features/create-request/ui/steps/task-step/task-step.tsx
+++ src/features/create-request/ui/steps/task-step/task-step.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import classNames from 'classnames';
 
 import { useAppDispatch, useAppSelector } from 'app/hooks';
