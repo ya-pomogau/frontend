307060ddddaa7d59b4d8656c4d27bb718619d4aa
Zdthns <Zdthns25@gmail.com>
2024-01-29 13:55:03 +0300

    финиш

--- src/features/create-request/ui/steps/address-step/address-step.tsx
+++ src/features/create-request/ui/steps/address-step/address-step.tsx
@@ -46,6 +46,20 @@ export const AddressStep = ({ isMobile }: IAddressProps) => {
 
   const propsButton = usePropsButtonCustom();
 
+  const handleSubmitClick = () => {
+    dispatch(closePopup());
+  };
+  const propsButtonDefault = {
+    label: 'Продолжить',
+    onClick: handleNextStepClick,
+  };
+  const propsEditButton = {
+    label: 'сохранить',
+    onClick: handleSubmitClick,
+  };
+
+  const propsButton = isTypeEdit ? propsEditButton : propsButtonDefault;
+
   return (
     <>
       <div className={styles.addressContainer}>
