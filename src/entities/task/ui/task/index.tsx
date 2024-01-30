8bc42230e84ba9890a3e15e68ea3c9740445568c
Zdthns <Zdthns25@gmail.com>
2024-01-30 11:46:54 +0300

    добавила id  task в кнопку

--- src/entities/task/ui/task/index.tsx
+++ src/entities/task/ui/task/index.tsx
@@ -576,6 +576,7 @@ export const TaskItem = ({
             />
           </ButtonWithModal>
           <SquareButton
+            taskId={taskId}
             onClick={handleEditButton}
             buttonType="edit"
             extClassName={
