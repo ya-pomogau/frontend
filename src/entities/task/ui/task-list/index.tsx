b8c0ae25bbd93aa1526a37e645ef3627665db96a
Zdthns <Zdthns25@gmail.com>
2024-01-31 14:18:46 +0300

    подтягиваю данные из store в popup

--- src/entities/task/ui/task-list/index.tsx
+++ src/entities/task/ui/task-list/index.tsx
@@ -66,7 +66,6 @@ export const TaskList = ({
     }
     setIsOpen((prev) => !prev);
   };
-  console.log(tasks);
 
   useEffect(() => {
     window.addEventListener('resize', getCoords);
@@ -112,7 +111,6 @@ export const TaskList = ({
           {tasks.map((item, index) => (
             <li key={index}>
               <TaskItem
-                taskId={item.id}
                 category={item.category.name}
                 isMobile={isMobile}
                 date={item.date}
