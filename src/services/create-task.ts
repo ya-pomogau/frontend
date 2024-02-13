c314834045f926cbbe144eac09d0cdecf841863b
Alexandr Gerasimov <phantom6391@yandex.ru>
2024-02-13 21:10:53 +0400

    Запрос на создание таски

---
+++ src/services/create-task.ts
@@ -0,0 +1,66 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+import { TPointGeoJSON } from 'entities/user/types';
+
+interface CreateTaskDto {
+  categoryId: string;
+  location: TPointGeoJSON;
+  date: Date | null;
+  address: string;
+  description: string;
+}
+const token = localStorage.getItem('token');
+export const newTask = createAsyncThunk(
+  'recipient/create',
+  async (dto: CreateTaskDto) => {
+    const response = await fetch('http://localhost:3001/recipient/tasks', {
+      method: 'POST',
+      headers: {
+        //eslint-disable-next-line @typescript-eslint/naming-convention
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + token,
+      },
+      body: JSON.stringify(dto),
+    });
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error('failed create task');
+    }
+  }
+);
+
+const createTaskSlice = createSlice({
+  name: 'createTask',
+  initialState: {
+    task: null,
+    isLoading: false,
+    error: false,
+  },
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(newTask.pending, (state, _) => ({
+        ...state,
+        error: false,
+        isLoading: true,
+      }))
+      .addCase(newTask.fulfilled, (state, action) => {
+        if (!action.payload) {
+          return state;
+        }
+        return {
+          ...state,
+          task: action.payload,
+          error: false,
+          isLoading: false,
+        };
+      })
+      .addCase(newTask.rejected, (state) => ({
+        ...state,
+        error: true,
+        isLoading: false,
+      }));
+  },
+});
+export default createTaskSlice.reducer;
