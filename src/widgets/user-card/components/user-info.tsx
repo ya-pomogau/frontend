c1bb32bda2149f60ce649f54b85a3ec5c8301ffd
Anton <113182938+Anton-Laid@users.noreply.github.com>
2024-03-13 15:11:46 +0300

    fix: correction styles cards and scroll

--- src/widgets/user-card/components/user-info.tsx
+++ src/widgets/user-card/components/user-info.tsx
@@ -1,5 +1,6 @@
 import classnames from 'classnames';
 import styles from '../styles.module.css';
+import classes from '../styles.module.css';
 import { UserRole } from '../../../shared/types/common.types';
 
 interface UserInfoProps {
@@ -21,13 +22,41 @@ const UserInfo = ({ role, userName, userId, userNumber }: UserInfoProps) => (
     </h2>
     {role === UserRole.RECIPIENT && (
       <div className={classnames(styles.grid_two, styles.id_color)}>
-        <p className="m-0 text text_size_small text_type_regular"> ID </p>
-        <p className="m-0 text text_size_small text_type_regular">{userId}</p>
+        <p
+          className={classnames(
+            styles.id,
+            'm-0 text text_size_small text_type_regular'
+          )}
+        >
+          ID
+        </p>
+        <p
+          className={classnames(
+            classes.id,
+            'm-0 text text_size_small text_type_regular'
+          )}
+        >
+          {userId}
+        </p>
       </div>
     )}
     <div className={styles.grid_two}>
-      <p className="m-0 text text_size_small text_type_bold "> тел: </p>
-      <p className="m-0 text text_size_small text_type_regular">{userNumber}</p>
+      <p
+        className={classnames(
+          classes.tel,
+          'm-0 text text_size_small text_type_regular text_type_bold '
+        )}
+      >
+        тел:
+      </p>
+      <p
+        className={classnames(
+          classes.tel,
+          'm-0 text text_size_small text_type_regular text_type_regular '
+        )}
+      >
+        {userNumber}
+      </p>
     </div>
   </div>
 );
