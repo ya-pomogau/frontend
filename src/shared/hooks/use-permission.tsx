6a845a27dc9d554628e557897bf8fdc7e31ce24f
ivanna-balanyuk <ivannabalanyuk@yandex.ru>
2024-06-12 14:23:26 +0300

    fix: вернуть usePermission к исходному состоянию

--- src/shared/hooks/use-permission.tsx
+++ src/shared/hooks/use-permission.tsx
@@ -28,7 +28,6 @@ export default function usePermission(
   requirments: Array<Requirments>,
   role: Role | null
 ) {
-  const isRootUser = useAppSelector((state) => state.user.data?.isRoot);
   const userStatus = useAppSelector((state) => state.user.data?.status);
   const userRole = useAppSelector((state) => state.user.data?.role);
   const userPermissions = useAppSelector(
@@ -37,22 +36,18 @@ export default function usePermission(
 
   let isAllowed = false;
 
-  if (isRootUser) {
-    isAllowed = true;
-  } else {
-    if (userRole === role) {
-      if (userRole) {
-        const userRights = { status: userStatus, ...userPermissions };
-        const hasPermission = requirments.filter((requirment) =>
-          Object.values(userRights).some(
-            (element) => JSON.stringify(element) === JSON.stringify(requirment)
-          )
-        );
-        hasPermission.length > 0 ? (isAllowed = true) : (isAllowed = false);
-      }
-    } else {
-      isAllowed = false;
+  if (userRole === role) {
+    if (userRole) {
+      const userRights = { status: userStatus, ...userPermissions };
+      const hasPermission = requirments.filter((requirment) =>
+        Object.values(userRights).some(
+          (element) => JSON.stringify(element) === JSON.stringify(requirment)
+        )
+      );
+      hasPermission.length > 0 ? (isAllowed = true) : (isAllowed = false);
     }
+  } else {
+    isAllowed = false;
   }
 
   return isAllowed;
