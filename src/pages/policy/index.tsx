2278b7124e8caf9424bfcd34e31e4e26c0edea7f
Denis Latyshev <d.d.latyshev@yandex.ru>
2024-06-26 15:13:24 +0300

    fix: - добавлена возможность редактирования страницы политики конфиденциальности

--- src/pages/policy/index.tsx
+++ src/pages/policy/index.tsx
@@ -1,17 +1,102 @@
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
-import { markdown } from './content';
+import {
+  markdown,
+  initTitleMarkdown,
+  initDescriptionMarkdown,
+} from './content';
 import style from './markdown-style.module.css';
+import { EditIcon } from '../../shared/ui/icons/edit-icon';
+import React, { SyntheticEvent, useState } from 'react';
+import { Button } from '../../shared/ui/button';
+import { CloseIconThin } from '../../shared/ui/icons/close-icon-thin';
 
 export function PolicyPage() {
+  // Нужно реализовать хранение значений titleMarkdown и descriptionMarkdown на сервере в базе данных
+  const [titleMarkdown, setTitleMarkdown] = useState(initTitleMarkdown);
+  const [descriptionMarkdown, setDescriptionMarkdown] = useState(
+    initDescriptionMarkdown
+  );
+  const [titleInput, setTitleInput] = useState(initTitleMarkdown);
+  const [descriptionInput, setDescriptionInput] = useState(
+    initDescriptionMarkdown
+  );
+  const [editState, setEditState] = useState(false);
+  const handleEditButton = () => {
+    setEditState(true);
+  };
+  const handleCloseButton = () => {
+    setEditState(false);
+  };
+  const onChangeTitleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTitleInput(event.target.value);
+  };
+  const onChangeDescriptionInput = (
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    setDescriptionInput(event.target.value);
+  };
+  const onSubmit = (event: SyntheticEvent) => {
+    event.preventDefault();
+    setTitleMarkdown(titleInput);
+    setDescriptionMarkdown(descriptionInput);
+    setEditState(false);
+  };
   return (
     <div className={style.wrapper}>
-      <ReactMarkdown
-        className={style.markdownStyles}
-        remarkPlugins={[remarkGfm]}
-      >
-        {markdown}
-      </ReactMarkdown>
+      {!editState && (
+        <>
+          <div className={style.title}>
+            <ReactMarkdown
+              className={style.markdownStyles}
+              remarkPlugins={[remarkGfm]}
+            >
+              {titleMarkdown}
+            </ReactMarkdown>
+            <button className={style.editButton} onClick={handleEditButton}>
+              <EditIcon color={'blue'} size={'24'} />
+              <p className={style.editButtonText}>Редактировать</p>
+            </button>
+          </div>
+          <ReactMarkdown
+            className={style.markdownStyles}
+            remarkPlugins={[remarkGfm]}
+          >
+            {descriptionMarkdown}
+          </ReactMarkdown>
+        </>
+      )}
+      {editState && (
+        <form className={style.editForm} onSubmit={onSubmit}>
+          <p className={style.editTitleText}>Заголовок</p>
+          <input
+            type={'text'}
+            name={'title'}
+            value={titleInput}
+            className={style.titleInput}
+            onChange={onChangeTitleInput}
+          />
+          <p className={style.editDescriptionText}>Текст</p>
+          <textarea
+            className={style.descriptionInput}
+            name={'description'}
+            value={descriptionInput}
+            onChange={onChangeDescriptionInput}
+          ></textarea>
+          <div className={style.buttonContainer}>
+            <Button
+              buttonType="primary"
+              actionType="submit"
+              label="Опубликовать"
+              size="extraLarge"
+            />
+            <button className={style.closeButton} onClick={handleCloseButton}>
+              <CloseIconThin color={'blue'}></CloseIconThin>
+              <p className={style.closeButtonText}>Закрыть без изменений</p>
+            </button>
+          </div>
+        </form>
+      )}
     </div>
   );
 }
