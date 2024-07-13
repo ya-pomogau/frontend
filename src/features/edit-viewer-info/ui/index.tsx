3b36ab271cd49a0c448ebf349a3cffacbbed4d0d
Alexander <oops.krit@yandex.ru>
2024-07-13 14:21:36 +0300

    fix: EditViewerInfo styles and structure

--- src/features/edit-viewer-info/ui/index.tsx
+++ src/features/edit-viewer-info/ui/index.tsx
@@ -1,79 +1,64 @@
-import { useRef, useEffect } from 'react';
-import { useForm, Controller } from 'react-hook-form';
+import { useEffect, useRef } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import classnames from 'classnames';
 
 import { Avatar } from '../../../shared/ui/avatar';
 import { Button } from '../../../shared/ui/button';
-import { Input } from '../../../shared/ui/input';
+import { FormInput } from '../../../shared/ui/form-input';
 
-import { CloseCrossIcon } from 'shared/ui/icons/close-cross-icon';
 import { LightPopup } from 'shared/ui/light-popup';
-import { useOutsideClick } from 'shared/hooks/use-outside-click';
-
-import type { UpdateUserInfo } from 'entities/user/types';
 
 import styles from './edit-viewer-info.module.css';
 
+interface EditViewerInfoForm {
+  name: string;
+  phone: string;
+  address: string;
+}
+
 interface EditViewerInfoProps {
   extClassName?: string;
-  avatarLink: string;
-  avatarName: string;
-  onClickSave: (userData: Omit<UpdateUserInfo, '_id'>) => void;
-  valueName: string;
-  valuePhone: string;
-  valueAddress: string;
-  valueId: string;
-  isPopupOpen: boolean;
-  buttonRef: React.RefObject<HTMLElement>;
-  isFormSaved: boolean;
-  setIsFormSaved: React.Dispatch<React.SetStateAction<boolean>>;
-  setIsPopupOpen: (isPopupOpen: boolean) => void;
-  isFormEdited: boolean;
-  setIsFormEdited: (isFormEdited: boolean) => void;
-  image: string;
-  setImage: (image: string) => void;
+  userAvatar: string;
+  userName: string;
+  userPhone: string;
+  userAddress: string;
+  onSave: SubmitHandler<EditViewerInfoForm>;
+  isOpen: boolean;
+  onClose: () => void;
 }
 
 export const EditViewerInfo = ({
+  userName,
+  userPhone,
+  userAddress,
+  userAvatar,
+  onSave,
+  isOpen,
+  onClose,
   extClassName,
-  avatarLink,
-  avatarName,
-  onClickSave,
-  valueName,
-  valuePhone,
-  valueAddress,
-  isPopupOpen,
-  buttonRef,
-  isFormSaved,
-  setIsFormSaved,
-  setIsPopupOpen,
-  setIsFormEdited,
-  image,
-  setImage,
 }: EditViewerInfoProps) => {
   const {
     handleSubmit,
     control,
     formState: { errors },
     reset,
-    watch,
   } = useForm({
     defaultValues: {
-      name: valueName,
-      phone: valuePhone,
-      address: valueAddress,
+      name: userName,
+      phone: userPhone,
+      address: userAddress,
     },
+    mode: 'onChange',
   });
 
   const avatarPicker = useRef<HTMLInputElement>(null);
-  const modalRef = useRef<HTMLDivElement>(null);
 
-  const onSubmit = (data: Omit<UpdateUserInfo, '_id'>) => {
-    onClickSave(data);
-    setIsFormSaved(true);
-    setIsFormEdited(false);
-    setIsPopupOpen(false);
+  const onSubmit: EditViewerInfoProps['onSave'] = (data) => {
+    onSave(data);
+    onClose();
   };
+
+  useEffect(() => reset(), [isOpen]);
 
   const handlePick = () => {
     if (avatarPicker.current) {
@@ -81,218 +66,113 @@ export const EditViewerInfo = ({
     }
   };
 
-  const handleClosePopup = () => {
-    setImage('');
-    setIsFormEdited(false);
-    setIsPopupOpen(false);
-    reset();
-  };
+  const inputLabelStyles = classnames(
+    styles.infoBlock__text,
+    'text',
+    'text_size_small',
+    'm-0',
+    'p-0',
+    'text_type_bold'
+  );
 
-  useOutsideClick({
-    elementRef: modalRef,
-    triggerRef: buttonRef,
-    onOutsideClick: handleClosePopup,
-    enabled: isPopupOpen,
-  });
-
-  useEffect(() => {
-    document.addEventListener('keydown', handleEscKeydown);
-
-    return () => {
-      document.removeEventListener('keydown', handleEscKeydown);
-    };
-  }, [isFormSaved]);
-
-  const handleEscKeydown = (e: KeyboardEvent) => {
-    e.key === 'Escape' && handleClosePopup();
-  };
-
-  useEffect(() => {
-    setIsFormEdited(true);
-    setIsFormSaved(false);
-  }, [watch('name'), watch('phone'), watch('address')]);
+  const popupStyles = classnames(styles.container, extClassName);
 
   return (
-    <LightPopup isPopupOpen={isPopupOpen} onClickExit={handleClosePopup}>
-      <div
-        ref={modalRef}
-        className={classnames(
-          styles.container,
-          extClassName,
-          styles.editViewerPopup
-        )}
-      >
-        <div className={styles.containerInfo}>
-          <div className={styles.headerElements}>
-            <div className={styles.avatarBlock}>
-              {avatarLink && !image && (
-                <Avatar
-                  extClassName={styles.avatar}
-                  avatarLink={avatarLink}
-                  avatarName={avatarName}
-                />
-              )}
-              {image && (
-                <Avatar
-                  extClassName={styles.avatar}
-                  avatarLink={image}
-                  avatarName={image}
-                />
-              )}
-              <button
-                onClick={handlePick}
-                className={classnames(
-                  styles.avatarBlock__button,
-                  'text',
-                  'text_size_small'
-                )}
-                type="button"
-              >
-                Изменить фото
-              </button>
-              <input
-                disabled
-                className={classnames(
-                  styles.avatarBlock__hidden,
-                  'text',
-                  'text_size_small'
-                )}
-                placeholder="Изменить фото"
-                type="file"
-                name="avatar"
-                accept="image/*,.png,.jpg,.gif,.web,"
-                ref={avatarPicker}
-              />
-            </div>
-            <CloseCrossIcon
-              className={styles.closeIcon}
-              size="14"
-              color="blue"
-              onClick={handleClosePopup}
-            />
-          </div>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <ul className={classnames(styles.infoBlock, 'list')}>
-              <li className={styles.infoBlock__item}>
-                <p
-                  className={classnames(
-                    styles.infoBlock__text,
-                    'text',
-                    'text_size_small',
-                    'm-0',
-                    'p-0',
-                    'text_type_bold'
-                  )}
-                >
-                  {' '}
-                  Имя:{' '}
-                </p>
-                <Controller
-                  name="name"
-                  control={control}
-                  rules={{ required: 'Пустое поле' }}
-                  render={({ field }) => (
-                    <Input
-                      type="text"
-                      extClassName={styles.input}
-                      placeholder="Введите имя"
-                      {...field}
-                    />
-                  )}
-                />
-                {errors.name && (
-                  <span className={styles.errorText}>
-                    {errors.name.message}
-                  </span>
-                )}
-              </li>
-              <li className={styles.infoBlock__item}>
-                <p
-                  className={classnames(
-                    styles.infoBlock__text,
-                    'text',
-                    'text_size_small',
-                    'm-0',
-                    'p-0',
-                    'text_type_bold'
-                  )}
-                >
-                  {' '}
-                  Тел.:{' '}
-                </p>
-                <Controller
-                  name="phone"
-                  control={control}
-                  rules={{
-                    required: 'Пустое поле',
-                    pattern: {
-                      value: /^[+]7\d{10}$/,
-                      message: 'Неверный формат номера',
-                    },
-                  }}
-                  render={({ field }) => (
-                    <Input
-                      type="tel"
-                      extClassName={classnames(
-                        styles.input,
-                        errors.phone && styles['input--error']
-                      )}
-                      placeholder="Введите телефон"
-                      {...field}
-                    />
-                  )}
-                />
-                {errors.phone && (
-                  <span className={styles.errorText}>
-                    {errors.phone.message}
-                  </span>
-                )}
-              </li>
-              <li className={styles.infoBlock__item}>
-                <p
-                  className={classnames(
-                    styles.infoBlock__text,
-                    'text',
-                    'text_size_small',
-                    'm-0',
-                    'p-0',
-                    'text_type_bold'
-                  )}
-                >
-                  {' '}
-                  Адрес:{' '}
-                </p>
-                <Controller
-                  name="address"
-                  control={control}
-                  rules={{ required: 'Пустое поле' }}
-                  render={({ field }) => (
-                    <Input
-                      type="text"
-                      extClassName={styles.input}
-                      placeholder="Введите адрес"
-                      {...field}
-                    />
-                  )}
-                />
-                {errors.address && (
-                  <span className={styles.errorText}>
-                    {errors.address.message}
-                  </span>
-                )}
-              </li>
-            </ul>
-            <Button
-              type="submit"
-              disabled={Object.keys(errors).length > 0}
-              extClassName={styles.button}
-              buttonType="primary"
-              label="Сохранить"
-              size="medium"
-            />
-          </form>
+    <LightPopup
+      extClassName={popupStyles}
+      hasCloseButton
+      isPopupOpen={isOpen}
+      onClickExit={onClose}
+    >
+      <div className={styles.headerElements}>
+        <div className={styles.avatarBlock}>
+          <Avatar
+            extClassName={styles.avatar}
+            avatarLink={userAvatar}
+            avatarName={`Аватар пользователя ${userName}`}
+          />
+          <button
+            onClick={handlePick}
+            className={classnames(
+              styles.avatarBlock__button,
+              'text',
+              'text_size_small'
+            )}
+            type="button"
+          >
+            Изменить фото
+          </button>
+          <input
+            disabled
+            className={classnames(
+              styles.avatarBlock__hidden,
+              'text',
+              'text_size_small'
+            )}
+            placeholder="Изменить фото"
+            type="file"
+            name="avatar"
+            accept="image/*,.png,.jpg,.gif,.web,"
+            ref={avatarPicker}
+          />
         </div>
       </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <ul className={classnames(styles.infoBlock, 'list')}>
+          <li className={styles.infoBlock__item}>
+            <p className={inputLabelStyles}>Имя:</p>
+            <FormInput
+              type="text"
+              name="name"
+              rules={{
+                required: {
+                  value: true,
+                  message: 'Имя обязательно',
+                },
+              }}
+              control={control}
+              extClassName={styles.input}
+              placeholder="Введите имя"
+            />
+          </li>
+          <li className={styles.infoBlock__item}>
+            <p className={inputLabelStyles}>Тел.:</p>
+            <FormInput
+              type="tel"
+              name="phone"
+              rules={{
+                required: 'Телефон обязателен',
+                pattern: {
+                  value: /^([+]7|8)\d{10}$/,
+                  message: 'Неверный формат номера',
+                },
+              }}
+              control={control}
+              extClassName={styles.input}
+              placeholder="Введите телефон"
+            />
+          </li>
+          <li className={styles.infoBlock__item}>
+            <p className={inputLabelStyles}>Адрес:</p>
+            <FormInput
+              type="text"
+              name="address"
+              rules={{ required: 'Адрес обязателен' }}
+              control={control}
+              extClassName={styles.input}
+              placeholder="Введите адрес"
+            />
+          </li>
+        </ul>
+        <Button
+          type="submit"
+          disabled={Object.keys(errors).length > 0}
+          extClassName={styles.button}
+          buttonType="primary"
+          label="Сохранить"
+          size="medium"
+        />
+      </form>
     </LightPopup>
   );
 };
