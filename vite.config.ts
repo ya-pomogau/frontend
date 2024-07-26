42416a4f068c4e49067cd8112a180765c326c119
arty gvozdenkov <gvozdenkov@gmail.com>
2024-07-26 13:59:24 +0300

    chore(env): load env in vite.config.ts & add env examples

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
-  build: {
-    outDir: './build',
+export default defineConfig(({ command, mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), '') };
+
+  // dev specific config
+  if (command === 'serve') {
+    return {
+      plugins: [tsconfigPaths(), react()],
+      build: {
+        outDir: './build',
+      },
+      server: {
+        host: true,
+        open: false,
+        port: +process.env.VITE_PORT!,
+      },
+    };
+  } else {
+    // command === 'build'
+    return {
+      // build specific config
+    };
   }
-})
+});
