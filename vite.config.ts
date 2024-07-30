a3cea021ec41fde71fea606564eb5342e9441741
Alexey Krutyakov <coder.arctik@gmail.com>
2024-07-31 00:23:00 +0300

    fix: change vite.config.ts as in develop branch

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,30 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
-  build: {
-    outDir: './build',
-  },
-  test: {
-    environment: 'jsdom',
-    setupFiles: ['./vitest.setup.ts'],
-  },
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), '') };
+
+  // dev specific config
+  if (command === 'serve') {
+    return {
+      plugins: [tsconfigPaths(), react()],
+      build: {
+        outDir: './build',
+      },
+      server: {
+        host: true,
+        open: false,
+        port: +process.env.VITE_PORT!,
+      },
+    };
+  } else {
+    // command === 'build'
+    return {
+      // build specific config
+    };
+  }
 });
