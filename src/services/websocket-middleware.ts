f13e2306e7fa9d445f778810186891bf06c7100c
Alexander <oops.krit@yandex.ru>
2024-08-08 19:51:21 +0300

    fix: off ws

--- src/services/websocket-middleware.ts
+++ src/services/websocket-middleware.ts
@@ -25,60 +25,60 @@ export const websocketMiddleware: Middleware = (store) => {
   let socket: Socket;
 
   return (next) => (action) => {
-    const { dispatch } = store;
-    const token = getTokenAccess();
-
-    const isSocketConnected =
-      socket &&
-      store.getState().system.socketConnectionStatus ===
-        SocketConnectionStatus.CONNECTED;
-
-    if (!socket && actions.startSocketConnection.match(action)) {
-      console.log(`-> Starting connection to socket on ${API_HOST}:`, action);
-
-      socket = io(API_HOST, {
-        extraHeaders: {
-          authorization: token as string,
-        },
-      });
-
-      socket.on(SocketEvent.CONNECT, () => {
-        console.log(`-> Connected to socket on ${API_HOST}`);
-        dispatch(
-          actions.setSocketConnectionStatus(SocketConnectionStatus.CONNECTED)
-        );
-      });
-
-      socket.on(SocketEvent.CONNECT_USER_MESSAGE, ({ data }) => {
-        console.log(`-> The server has a message for you: ${data.message}`);
-        dispatch(actions.setSocketMessage(data));
-      });
-
-      socket.emit(SocketEvent.TEST, testEventObj);
-    }
-
-    if (isSocketConnected) {
-      socket.on(SocketEvent.MESSAGE, ({ data }) => {
-        console.log(`-> The server has a message for you: ${data.message}`);
-        dispatch(actions.setSocketMessage(data));
-      });
-
-      socket.on(SocketEvent.CONNECT_ERROR, (error) => {
-        console.log(`-> Connection error: ${error.message}`);
-      });
-
-      if (actions.closeSocketConnection.match(action)) {
-        console.log(`-> Socket connection is closing`);
-        socket.disconnect();
-      }
-
-      socket.on(SocketEvent.DISCONNECT, (reason) => {
-        dispatch(
-          actions.setSocketConnectionStatus(SocketConnectionStatus.DISCONNECTED)
-        );
-        console.log(`-> Socket connection was dropped: ${reason}`);
-      });
-    }
+    // const { dispatch } = store;
+    // const token = getTokenAccess();
+    //
+    // const isSocketConnected =
+    //   socket &&
+    //   store.getState().system.socketConnectionStatus ===
+    //     SocketConnectionStatus.CONNECTED;
+    //
+    // if (!socket && actions.startSocketConnection.match(action)) {
+    //   console.log(`-> Starting connection to socket on ${API_HOST}:`, action);
+    //
+    //   socket = io(API_HOST, {
+    //     extraHeaders: {
+    //       authorization: token as string,
+    //     },
+    //   });
+    //
+    //   socket.on(SocketEvent.CONNECT, () => {
+    //     console.log(`-> Connected to socket on ${API_HOST}`);
+    //     dispatch(
+    //       actions.setSocketConnectionStatus(SocketConnectionStatus.CONNECTED)
+    //     );
+    //   });
+    //
+    //   socket.on(SocketEvent.CONNECT_USER_MESSAGE, ({ data }) => {
+    //     console.log(`-> The server has a message for you: ${data.message}`);
+    //     dispatch(actions.setSocketMessage(data));
+    //   });
+    //
+    //   socket.emit(SocketEvent.TEST, testEventObj);
+    // }
+    //
+    // if (isSocketConnected) {
+    //   socket.on(SocketEvent.MESSAGE, ({ data }) => {
+    //     console.log(`-> The server has a message for you: ${data.message}`);
+    //     dispatch(actions.setSocketMessage(data));
+    //   });
+    //
+    //   socket.on(SocketEvent.CONNECT_ERROR, (error) => {
+    //     console.log(`-> Connection error: ${error.message}`);
+    //   });
+    //
+    //   if (actions.closeSocketConnection.match(action)) {
+    //     console.log(`-> Socket connection is closing`);
+    //     socket.disconnect();
+    //   }
+    //
+    //   socket.on(SocketEvent.DISCONNECT, (reason) => {
+    //     dispatch(
+    //       actions.setSocketConnectionStatus(SocketConnectionStatus.DISCONNECTED)
+    //     );
+    //     console.log(`-> Socket connection was dropped: ${reason}`);
+    //   });
+    // }
 
     next(action);
   };
