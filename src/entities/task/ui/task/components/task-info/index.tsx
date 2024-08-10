7d7d38d33a2a709241155963643112b2c645b857
Alexander <oops.krit@yandex.ru>
2024-08-10 14:08:33 +0300

    refactor: task info component

--- src/entities/task/ui/task/components/task-info/index.tsx
+++ src/entities/task/ui/task/components/task-info/index.tsx
@@ -1,8 +1,9 @@
-import { Icon } from 'shared/ui/icons';
-import styles from './styles.module.css';
 import classNames from 'classnames';
 import { format } from 'date-fns';
-import { useMediaQuery } from 'shared/hooks';
+
+import { Icon } from 'shared/ui';
+
+import styles from './styles.module.css';
 
 interface TaskInfoProps {
   date: string | null;
@@ -11,7 +12,9 @@ interface TaskInfoProps {
 }
 
 export const TaskInfo = ({ date, address, extClassName }: TaskInfoProps) => {
-  const isMobile = useMediaQuery('(max-width:1150px)');
+  const [currentDate, currentTime] = date
+    ? [format(new Date(date), 'dd.MM.yyyy'), format(new Date(date), 'HH:mm')]
+    : ['бессрочно', 'бессрочно'];
 
   return (
     <div className={classNames(extClassName, styles.taskInfo)}>
@@ -22,18 +25,18 @@ export const TaskInfo = ({ date, address, extClassName }: TaskInfoProps) => {
           size="24"
           className={styles.icon}
         />
-        <span>{date ? format(new Date(date), 'dd.MM.yyyy') : 'бессрочно'}</span>
+        <span>{currentDate}</span>
       </div>
       <div className={styles.time}>
         <Icon color="blue" icon="ClockIcon" size="24" className={styles.icon} />
-        <span>{date ? format(new Date(date), 'HH:mm') : 'бессрочно'}</span>
+        <span>{currentTime}</span>
       </div>
       <div className={styles.address}>
         <Icon
           color="blue"
           icon="LocationIcon"
           size="24"
-          className={classNames(styles.icon, styles.iconLocation)}
+          className={`${styles.icon} ${styles.iconLocation}`}
         />
         <span>{address}</span>
       </div>
