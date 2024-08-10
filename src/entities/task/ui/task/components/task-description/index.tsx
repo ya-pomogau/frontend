dc82edd405af0458a54f26c4279e8abff3128af8
Alexander <oops.krit@yandex.ru>
2024-08-10 14:08:07 +0300

    refactor: task description component

--- src/entities/task/ui/task/components/task-description/index.tsx
+++ src/entities/task/ui/task/components/task-description/index.tsx
@@ -1,7 +1,10 @@
-import { useLayoutEffect, useRef, useState } from 'react';
-import styles from './styles.module.css';
-import { Icon } from 'shared/ui/icons';
 import classNames from 'classnames';
+import { useRef } from 'react';
+
+import { useTruncatedText } from 'shared/hooks';
+import { Icon } from 'shared/ui';
+
+import styles from './styles.module.css';
 
 interface TaskDescriptionProps {
   description: string;
@@ -9,70 +12,31 @@ interface TaskDescriptionProps {
   extClassName?: string;
 }
 
-const expandedClass = styles.taskExpanded;
-
 export const TaskDescription = ({
   description,
   count,
   extClassName,
 }: TaskDescriptionProps) => {
   const textRef = useRef<HTMLParagraphElement>(null);
-  const [isExpanded, setIsExpanded] = useState(false);
-  const [isButtonVisible, setIsButtonVisible] = useState(false);
+  const { isTruncated, isExpanded, toggleIsShowingMore } =
+    useTruncatedText(textRef);
 
-  const setButtonVisibility = (expandable: HTMLParagraphElement) => {
-    if (expandable.scrollHeight > expandable.clientHeight) {
-      setIsButtonVisible(true);
-    } else {
-      setIsButtonVisible(false);
-    }
-  };
-
-  const handleExpandClick = () => {
-    if (textRef.current) {
-      setIsExpanded(!isExpanded);
-    }
-  };
-
-  useLayoutEffect(() => {
-    const handleResize = () => {
-      if (textRef.current) {
-        textRef.current.classList.remove(expandedClass);
-        setIsExpanded(false);
-        setButtonVisibility(textRef.current);
-      }
-    };
-
-    window.addEventListener('resize', handleResize);
-
-    handleResize();
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
+  const textStyles = classNames(styles.card__task, {
+    [styles.taskExpanded]: isExpanded,
+  });
 
   return (
     <div className={classNames(extClassName, styles.taskDescription)}>
       <div className={styles.card__expandable}>
-        <p
-          ref={textRef}
-          className={classNames(styles.card__task, {
-            [expandedClass]: isExpanded,
-          })}
-        >
+        <p ref={textRef} className={textStyles}>
           {description}
         </p>
-        {isButtonVisible && (
-          <button
-            onClick={handleExpandClick}
-            className={classNames(styles.expandBtn, 'text')}
-          >
+        {isTruncated && (
+          <button onClick={toggleIsShowingMore} className={styles.expandBtn}>
             {isExpanded ? 'Свернуть' : 'Читать'}
           </button>
         )}
       </div>
-
       <div className={styles.score}>
         <Icon color="blue" icon="BallsIcon" size="46" />
         <span className={styles.scoreText}>{count}</span>
