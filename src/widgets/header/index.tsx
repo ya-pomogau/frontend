1b5af70c774924b8fb4866fff5be3f9437f62816
Irina Syrcova <irinasyrcova223@gmail.com>
2024-08-10 17:10:16 +0300

    refactor: header-component

--- src/widgets/header/index.tsx
+++ src/widgets/header/index.tsx
@@ -1,11 +1,10 @@
 import { SyntheticEvent, useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import cn from 'classnames';
 
 import { useAppSelector } from 'app/hooks';
 import { useMediaQuery } from 'shared/hooks/media-query';
 import { Logo, Avatar, Button } from 'shared/ui';
-import { MenuIcon } from 'shared/ui/icons/menu-icon';
-import { UnionIcon } from 'shared/ui/icons/union-icon';
 import { handleRedirectVK } from 'shared/libs/utils';
 import { PopupChat } from 'entities/chat/ui/chat';
 import { infoAdmin } from 'entities/chat/ui/chat/libs/utils';
@@ -13,8 +12,10 @@ import { UserRole } from 'shared/types/common.types';
 import { SideBar } from './navigation';
 import { DropDownMenu } from './DropDownMenu';
 import { linksTop, linksTopAuthAdmin, positionConfigTop } from './utils';
-
+import { Routes } from 'shared/config';
 import defaultAvatar from 'shared/ui/info-container/img/placeholder.svg';
+import { MenuButton } from './components';
+
 import styles from './styles.module.css';
 
 const Header = () => {
@@ -23,6 +24,7 @@ const Header = () => {
 
   const isMobile = useMediaQuery('(max-width: 920px)');
   const user = useAppSelector((state) => state.user.data);
+  const isAdmin = user && user.role === UserRole.ADMIN;
 
   const handleClick = (evt: SyntheticEvent) => {
     evt.stopPropagation();
@@ -34,41 +36,39 @@ const Header = () => {
   };
 
   const isMenuHidden = !user && !isMobile;
+  const headerStyles = cn(styles.header, { [styles.header_mobile]: isMobile });
+  const headerLogoStyles = cn(styles.header__logo, {
+    [styles.header__logo_mobile]: isMobile,
+  });
+  const headerMenuStyles = cn(styles.header__menu__container, {
+    [styles.header__menu__container_hidden]: isMenuHidden,
+  });
 
   return (
-    <header className={`${styles.header} ${isMobile && styles.header_mobile}`}>
+    <header className={headerStyles}>
       <div className={styles.header__container}>
         {isMobile && user && (
-          <div className={`${styles.header__avatar} `}>
-            <Avatar
-              extClassName={styles.header__avatar}
-              avatarName={user.name}
-              avatarLink={user.avatar || defaultAvatar}
-            />
-          </div>
+          <Avatar
+            extClassName={styles.header__avatar}
+            avatarName={user.name}
+            avatarLink={user.avatar || defaultAvatar}
+          />
         )}
         {isMobile && !user && (
-          <div className={`${styles.header__button} `}>
-            <Button
-              buttonType="primary"
-              actionType="submit"
-              label="Войти"
-              size="small"
-              onClick={() => handleRedirectVK()}
-            />
-          </div>
+          <Button
+            buttonType="primary"
+            actionType="submit"
+            label="Войти"
+            size="small"
+            onClick={() => handleRedirectVK()}
+          />
         )}
 
-        <NavLink
-          className={`${styles.header__logo} ${
-            isMobile && styles.header__logo_mobile
-          }`}
-          to="/"
-        >
+        <NavLink className={headerLogoStyles} to={Routes.ROOT}>
           <Logo />
         </NavLink>
 
-        {user && user.role === UserRole.ADMIN
+        {isAdmin
           ? !isMobile && (
               <SideBar position={positionConfigTop} links={linksTopAuthAdmin} />
             )
@@ -76,25 +76,9 @@ const Header = () => {
               <SideBar position={positionConfigTop} links={linksTop} />
             )}
 
-        <div
-          className={`${styles.header__menu__container} ${
-            isMenuHidden && styles.header__menu__container_hidden
-          }`}
-        >
-          <button
-            onClick={handleClick}
-            className={styles.header__button}
-            type="button"
-          >
-            {isMobile ? (
-              <MenuIcon color="blue" />
-            ) : (
-              <div className={styles.header__button__container}>
-                <span className={styles.header__button__text}>Меню</span>
-                <UnionIcon color="blue" />
-              </div>
-            )}
-          </button>
+        <div className={headerMenuStyles}>
+          <MenuButton onClick={handleClick} isMobile={isMobile} />
+
           {menuActive && (
             <DropDownMenu
               role={user?.role}
