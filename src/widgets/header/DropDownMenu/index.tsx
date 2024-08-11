6c872cac21d6851de04fda5c2a99a587a9146ddd
Sergei Gavrilov <iamgavr@yandex.ru>
2024-08-11 21:31:42 +0300

    refactor: styles organized, temporary solutions removed

--- src/widgets/header/DropDownMenu/index.tsx
+++ src/widgets/header/DropDownMenu/index.tsx
@@ -3,18 +3,19 @@
 import { useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 import { useNavigate } from 'react-router-dom';
+import cn from 'classnames';
 
 import { UserRole } from '../../../shared/types/common.types';
-import { useAppDispatch, useAppSelector } from '../../../app/hooks';
+import { useAppDispatch } from '../../../app/hooks';
 import { useMediaQuery } from '../../../shared/hooks';
-import { handleRedirectVK } from '../../../shared/libs/utils';
+import { Routes } from '../../../shared/config';
 import { logoutUser } from '../../../entities/user/model';
 import { closeSocketConnection } from '../../../services/system-slice';
+import useUser from 'shared/hooks/use-user';
+import { UnauthorizedUser } from 'entities/user/ui/user-info/unauthorized-user';
 
 import { SideBar } from '../../../widgets/header/navigation';
 import { DropDownMenuButton } from '../../../shared/ui/DropDownMenuButton';
-import { VkIcon } from '../../../shared/ui/icons/vk-icon';
-import { Button } from '../../../shared/ui/button';
 import { positionConfigMenu, linksTopAuthAdmin, linksTop } from '../utils';
 
 import styles from './styles.module.css';
@@ -36,7 +37,8 @@ export const DropDownMenu = ({
   const isMobile = useMediaQuery('(max-width: 920px)');
   const ref = useRef(null);
 
-  const user = useAppSelector((state) => state.user.data);
+  const isAuth = useUser();
+  const isAdmin = role === UserRole.ADMIN;
 
   const closeByOverlay = (evt: MouseEvent) => {
     if (evt.target !== ref.current) {
@@ -66,30 +68,20 @@ export const DropDownMenu = ({
   const handlerOnClick = () => {
     dispatch(logoutUser());
     dispatch(closeSocketConnection());
-    return navigate('/');
+    return navigate(Routes.ROOT);
   };
 
-  const line = {
-    background: '#E0E0E0',
-    height: '1px',
-    width: '90%',
-    marginLeft: '20px',
-    marginTop: role === 'Admin' ? '20px' : '0',
-  };
+  const lineStyles = cn(styles.line, { [styles.line_admin]: isAdmin });
+  const sidebarContainerStyles = cn(styles.header__sidebar__container, {
+    [styles.header__sidebar__container_mobile]: isMobile,
+  });
 
   return createPortal(
-    <div
-      className={
-        isMobile
-          ? styles.header__sidebar__container_mobile
-          : styles.header__sidebar__container
-      }
-      ref={ref}
-    >
-      {user ? (
+    <div className={sidebarContainerStyles} ref={ref}>
+      {isAuth ? (
         <div className={styles.wrapper}>
           {/* eslint-disable-next-line eqeqeq */}
-          {role != 'Admin' && (
+          {!isAdmin && (
             <DropDownMenuButton
               isMobile={isMobile}
               onClick={() => setIsOpenChat(true)}
@@ -101,25 +93,17 @@ export const DropDownMenu = ({
           {isMobile ? (
             <SideBar
               position={positionConfigMenu}
-              links={role === UserRole.ADMIN ? linksTopAuthAdmin : linksTop}
+              links={isAdmin ? linksTopAuthAdmin : linksTop}
             />
           ) : (
-            <div style={line}></div>
+            <div className={lineStyles}></div>
           )}
           <DropDownMenuButton isMobile={isMobile} onClick={handlerOnClick}>
             Выход
           </DropDownMenuButton>
         </div>
       ) : (
-        // TODO: временная мера на время тестирования клиентом.
-        <Button
-          buttonType="primary"
-          actionType="submit"
-          customIcon={<VkIcon color="white" size="24" />}
-          label="Войти через ВКонтакте"
-          size="medium"
-          onClick={() => handleRedirectVK()}
-        />
+        <UnauthorizedUser />
       )}
     </div>,
     modalRoot
