fcd5f69769c70af506facb36d320005bca0983e8
Kristina Khramova <snowflak@yandex.ru>
2024-08-12 20:49:19 +0300

    Добавлены комментарии к брейкпоинтам.

--- src/shared/config/breakpoints.ts
+++ src/shared/config/breakpoints.ts
@@ -1,6 +1,6 @@
 export const Breakpoints = {
-  IS_MOBILE_1150: '(max-width: 1150px)',
-  IS_MOBILE_MENU: '(max-width: 920px)',
-  IS_MOBILE_POPUP: '(max-width: 735px)',
-  IS_MOBILE_CHAT: '(max-width: 550px)',
+  IS_MOBILE_1150: '(max-width: 1150px)', // For general mobile view
+  IS_MOBILE_MENU: '(max-width: 920px)', // For mobile menu adjustments
+  IS_MOBILE_POPUP: '(max-width: 735px)', // For mobile popup adjustments
+  IS_MOBILE_CHAT: '(max-width: 550px)', // For mobile chat adjustments
 } as const;
