045e43a7eefb0886c6c1f1a586e2c2508905dc74
Alexander <oops.krit@yandex.ru>
2024-08-23 20:18:23 +0300

    fix: errors

--- src/shared/ui/icons/arrow-notify.tsx
+++ src/shared/ui/icons/arrow-notify.tsx
@@ -12,7 +12,7 @@ export const ArrowNotify = ({ ...props }: IIconProps) => (
     <path
       d="M86.5 1C131.3 40.2 36.6667 50.6667 4 59M4 59C8.5 55.6667 17 53 20 44M4 59C10.1667 58.5 23.3 58 26.5 60"
       stroke="#2E3192"
-      stroke-width="2"
+      strokeWidth="2"
     />
   </svg>
 );
