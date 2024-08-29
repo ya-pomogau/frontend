17d0845e62717ab71c59325570933e69b0d622f8
Elena Orekhova <lelyaorol@gmail.com>
2024-08-29 14:37:13 +0300

    fix checkbox to storybook

--- src/shared/ui/checkbox/index.tsx
+++ src/shared/ui/checkbox/index.tsx
@@ -1,4 +1,4 @@
-import { InputHTMLAttributes } from 'react';
+import { InputHTMLAttributes, useState, useEffect } from 'react';
 import classnames from 'classnames';
 
 import { DefaultCheckboxIcon } from './checkbox-icon';
@@ -8,6 +8,7 @@ import styles from './styles.module.css';
 interface CheckboxProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
   extClassName?: string;
+  defaultChecked?: boolean;
 }
 
 const Checkbox = ({
@@ -15,36 +16,52 @@ const Checkbox = ({
   id,
   onChange,
   checked,
+  defaultChecked,
   extClassName,
   name,
   ...props
-}: CheckboxProps) => (
-  <>
-    <input
-      className={styles.checkbox}
-      type="checkbox"
-      id={id}
-      name={name || id}
-      onChange={onChange}
-      checked={checked}
-      value={id}
-      {...props}
-    />
-    <label
-      htmlFor={id}
-      className={classnames('text', 'text_type_regular', extClassName)}
-    >
-      <div className={styles.tooltip}>
-        Вы пока не можете выбрать эту категорию
-      </div>
-      <div className={styles.checkboxIconWrapper}>
-        <span className={styles.checkboxIcon}>
-          <DefaultCheckboxIcon />
-        </span>
-      </div>
-      {label}
-    </label>
-  </>
-);
+}: CheckboxProps) => {
+  const [isChecked, setIsChecked] = useState(defaultChecked || false);
+
+  useEffect(() => {
+    setIsChecked(defaultChecked || false);
+  }, [defaultChecked]);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setIsChecked(event.target.checked);
+    if (onChange) {
+      onChange(event);
+    }
+  };
+
+  return (
+    <>
+      <input
+        className={styles.checkbox}
+        type="checkbox"
+        id={id}
+        name={name || id}
+        onChange={handleChange}
+        checked={checked ?? isChecked}
+        value={id}
+        {...props}
+      />
+      <label
+        htmlFor={id}
+        className={classnames('text', 'text_type_regular', extClassName)}
+      >
+        <div className={styles.tooltip}>
+          Вы пока не можете выбрать эту категорию
+        </div>
+        <div className={styles.checkboxIconWrapper}>
+          <span className={styles.checkboxIcon}>
+            <DefaultCheckboxIcon />
+          </span>
+        </div>
+        {label}
+      </label>
+    </>
+  );
+};
 
 export default Checkbox;
