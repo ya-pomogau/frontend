9259eea1856ce6cd18082259d125e7f720e9c5e6
Kristina Khramova <snowflak@yandex.ru>
2024-08-30 22:20:05 +0300

    Реализован лк для мобильной версии - верстка и переход по пункту меню

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,13 +1,30 @@
+import { useEffect, useState } from 'react';
 import { Navigate } from 'react-router-dom';
 
 import { useAppSelector } from 'app/hooks';
 import { isRootSelector } from 'entities/user/model';
 import { UserRole } from 'shared/types/common.types';
-import { Routes } from 'shared/config';
+import { Breakpoints, Routes } from 'shared/config';
+import { useMediaQuery } from 'shared/hooks';
 
 export function ProfilePage() {
   const role = useAppSelector((state) => state.user.role);
   const isRoot = useAppSelector(isRootSelector);
+  // const isMobile = useMediaQuery(Breakpoints.L)
+
+  const [isMobile, setIsMobile] = useState(window.matchMedia('(max-width: 920px)').matches);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 920px)');
+    const handler = (event:any) => setIsMobile(event.matches);
+    mediaQuery.addEventListener('change', handler);
+
+    return () => {
+      mediaQuery.removeEventListener('change', handler);
+    };
+  }, []);
+  
+  if (isMobile) return null;
 
   if (role === UserRole.VOLUNTEER) {
     return <Navigate to={Routes.PROFILE_MAP} replace />;
