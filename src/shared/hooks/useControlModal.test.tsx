2b12ffe68e53468df30a2f7d93c13d15bf9738a1
Александр Васянькин <henchfiend@yandex.ru>
2024-09-10 16:19:49 +0300

    тест для useControlModal
    - написан тест для проверки работы хука useControlModal

---
+++ src/shared/hooks/useControlModal.test.tsx
@@ -0,0 +1,22 @@
+import { act, renderHook } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+import { useControlModal } from './useControlModal';
+
+describe('check useControlModal', () => {
+  it('should return false when hook create', () => {
+    const { result } = renderHook(() => useControlModal());
+    expect(result.current.isOpen).toBe(false);
+  });
+
+  it('should return true when handleOpen called', () => {
+    const { result } = renderHook(() => useControlModal());
+    act(() => result.current.handleOpen());
+    expect(result.current.isOpen).toBe(true);
+  });
+
+  it('should return true when handleClose called', () => {
+    const { result } = renderHook(() => useControlModal());
+    act(() => result.current.handleClose());
+    expect(result.current.isOpen).toBe(false);
+  });
+});
