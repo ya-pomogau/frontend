8a4680ec1355476dfdba972b737a4a95483c59b1
Vladimir Kuleshov <Gottgunst@gmail.com>
2024-09-27 14:18:49 +0300

    fix: storybook dependency

--- src/entities/chat/ui/chat/libs/utils.ts
+++ src/entities/chat/ui/chat/libs/utils.ts
@@ -1,4 +1,11 @@
+import { mockChatMessages } from 'entities/chat/mock-messages';
+import { mockAdmin } from 'entities/chat/mock-users';
 import { MessageInterface } from 'shared/types/chat.types';
+import { AdminInterface } from 'shared/types/user.type';
+
+export const getMockMessages = (): MessageInterface[] => mockChatMessages;
+
+export const infoAdmin: AdminInterface = mockAdmin;
 
 export const sortMessages = (messages: MessageInterface[]) =>
   messages.sort((a, b) => (a.createdAt > b.createdAt ? 1 : -1));
