4d7ff50d4b5318ec4fbfdb9290ea56cfee7856f6
Alexander <oops.krit@yandex.ru>
2024-12-07 15:31:31 +0300

    fix: blog form

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import { nanoid } from '@reduxjs/toolkit';
 
 import { useDeletePostMutation, useGetPostsQuery } from 'services/posts-api';
@@ -14,7 +14,6 @@ import {
 } from 'shared/ui';
 import { useControlModal, usePermission } from 'shared/hooks';
 import { PostProps } from 'shared/ui/post/Post';
-import { IValuesBlog } from 'shared/types/blog.types';
 import { adminPermission, userRole } from 'shared/types/common.types';
 
 import styles from './styles.module.css';
@@ -30,16 +29,9 @@ export function BlogPage() {
   const [attachments, setAttachments] = useState<
     { file: File; id: string; name: string }[]
   >([]);
-  const [idEditedPost, setIdEditedPost] = useState<string | undefined>(
-    undefined
-  );
 
-  const [values, setValues] = useState<IValuesBlog>({
-    title: '',
-    text: '',
-  });
-
-  const refPostForm = useRef<HTMLFormElement>(null);
+  const [selectedPost, setSelectedPost] =
+    useState<Omit<PostProps, 'handleEditButton' | 'handleDeleteButton'>>();
 
   const handleAddAttachment = (fileList: FileList | null) => {
     if (!fileList) return;
@@ -57,29 +49,27 @@ export function BlogPage() {
     setAttachments(attachments.filter((attachment) => attachment.id !== id));
   };
 
-  const handleGetIdPost = async (id: string) => {
-    handleOpen();
-    setIdEditedPost(id);
-  };
-
   const handleDeletePost = async () => {
-    if (idEditedPost) {
-      await deletePost(idEditedPost);
+    if (selectedPost?._id) {
+      await deletePost(selectedPost._id);
     }
     handleClose();
   };
 
-  const handleEditPost = (post: Partial<PostProps>) => {
-    setValues({
-      title: post.title as string,
-      text: post.text as string,
-    });
+  const handleSelectPost = (
+    post: Omit<PostProps, 'handleEditButton' | 'handleDeleteButton'>,
+    isDelete?: boolean
+  ) => {
+    setSelectedPost(post);
 
-    setIdEditedPost(post._id);
+    if (isDelete) {
+      handleOpen();
+    }
+  };
 
-    refPostForm.current?.scrollIntoView({
-      behavior: 'smooth',
-    });
+  const handleCloseModal = () => {
+    setSelectedPost(undefined);
+    handleClose();
   };
 
   return (
@@ -92,19 +82,9 @@ export function BlogPage() {
         />
         {isAdmin && (
           <PostForm
-            handleSubmit={() => {
-              setValues({
-                title: '',
-                text: '',
-              });
-            }}
-            idEditedPost={idEditedPost}
-            refPostForm={refPostForm}
-            title={values.title}
-            text={values.text}
+            selectedPost={!isOpen ? selectedPost : undefined}
             addAttachment={handleAddAttachment}
             removeAttachment={handleRemoveAttachment}
-            images={attachments}
           />
         )}
 
@@ -120,8 +100,8 @@ export function BlogPage() {
                 text={text}
                 files={files}
                 author={author}
-                handleDeleteButton={isAdmin ? handleGetIdPost : undefined}
-                handleEditButton={isAdmin ? handleEditPost : undefined}
+                handleDeleteButton={isAdmin ? handleSelectPost : undefined}
+                handleEditButton={isAdmin ? handleSelectPost : undefined}
               />
             ))}
           </div>
@@ -129,7 +109,7 @@ export function BlogPage() {
         <LightPopup
           hasCloseButton={true}
           isPopupOpen={isOpen}
-          onClickExit={handleClose}
+          onClickExit={handleCloseModal}
           extClassName={styles.popup}
         >
           <Typography
--- src/shared/ui/post/Post.tsx
+++ src/shared/ui/post/Post.tsx
@@ -20,8 +20,13 @@ export interface PostProps {
   text: string;
   files: ImageProps[];
   author: Pick<User, '_id' | 'name' | 'avatar'>;
-  handleDeleteButton?: (id: string) => void;
-  handleEditButton?: (post: Partial<PostProps>) => void;
+  handleDeleteButton?: (
+    post: Omit<PostProps, 'handleEditButton' | 'handleDeleteButton'>,
+    isDelete: boolean
+  ) => void;
+  handleEditButton?: (
+    post: Omit<PostProps, 'handleEditButton' | 'handleDeleteButton'>
+  ) => void;
 }
 
 export const Post: FC<PostProps> = ({
@@ -104,7 +109,18 @@ export const Post: FC<PostProps> = ({
         <div className={styles.buttons}>
           {handleDeleteButton && (
             <SquareButton
-              onClick={() => handleDeleteButton(_id!)}
+              onClick={() =>
+                handleDeleteButton(
+                  {
+                    _id,
+                    title,
+                    text,
+                    files,
+                    author,
+                  },
+                  true
+                )
+              }
               buttonType={'close'}
             />
           )}
@@ -116,6 +132,7 @@ export const Post: FC<PostProps> = ({
                   title,
                   text,
                   files,
+                  author,
                 })
               }
               buttonType={'edit'}
